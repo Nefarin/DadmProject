58429d943b5084b78d3998f431829e09c0a8c5a4
Marek <nefrius@gmail.com>
2015-11-11 13:55:07 +0100

    A6

--- EKG_Project/App.xaml.cs
+++ EKG_Project/App.xaml.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Threading;
-using System.Windows;
+﻿using System.Windows;
 using EKG_Project.GUI;
 
 namespace EKG_Project
