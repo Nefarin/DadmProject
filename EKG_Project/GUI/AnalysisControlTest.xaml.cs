95ff46ccea395011a52eb4afabdb96d99163ddb3
Marek <nefrius@gmail.com>
2015-12-01 13:14:42 +0100

    Example to OxyPlot added (in testControl) - GUI check

--- EKG_Project/GUI/AnalysisControlTest.xaml.cs
+++ EKG_Project/GUI/AnalysisControlTest.xaml.cs
@@ -4,6 +4,8 @@ using System.Windows.Controls;
 using EKG_Project.Architecture;
 using EKG_Project.Architecture.ProcessingStates;
 using EKG_Project.Architecture.GUIMessages;
+using System.Collections.Generic;
+using OxyPlot;
 
 namespace EKG_Project.GUI
 {
@@ -15,6 +17,52 @@ namespace EKG_Project.GUI
     #endregion
     public partial class AnalysisControlTest : UserControl
     {
+        private TestPlots _testPlot;
+
+        public class TestPlots
+        {
+            private string _plotTitle;
+            private IList<DataPoint> _plotPoints;
+
+            public TestPlots()
+            {
+                this.PlotTitle = "Test";
+                this.PlotPoints = new List<DataPoint>
+                              {
+                                  new DataPoint(0, 4),
+                                  new DataPoint(10, 13),
+                                  new DataPoint(20, 15),
+                                  new DataPoint(30, 16),
+                                  new DataPoint(40, 12),
+                                  new DataPoint(50, 12)
+                              };
+            }
+            public IList<DataPoint> PlotPoints
+            {
+                get
+                {
+                    return _plotPoints;
+                }
+
+                set
+                {
+                    _plotPoints = value;
+                }
+            }
+            public string PlotTitle
+            {
+                get
+                {
+                    return _plotTitle;
+                }
+
+                set
+                {
+                    _plotTitle = value;
+                }
+            }
+        }
+
         private void exitButton_Click(object sender, RoutedEventArgs e)
         {
             _communication.SendGUIMessage(new Abort());
@@ -34,7 +82,10 @@ namespace EKG_Project.GUI
 
         private void runButton_Click(object sender, RoutedEventArgs e)
         {
+            _testPlot = new TestPlots();
+            TestPlot.DataContext = _testPlot;
             _communication.SendGUIMessage(new TestState());
         }
+
     }
 }
