5efb83bf82dcdcfe22fa872e44aec80238fc74f2
KrzysztofKaganiec <campnou@student.agh.edu.pl>
2015-12-10 22:46:44 +0100

    Empty _Data Classes added to project

---
+++ Modules/ECG_Baseline/ECG_Baseline_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EKG_Project.Modules.ECG_Baseline
+{
+    class ECG_Baseline_Data : IO.ECG_Data
+    {
+    }
+}
---
+++ Modules/T_Wave_Alt/T_Wave_Alt_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EKG_Project.Modules.T_Wave_Alt
+{
+    class T_Wave_Alt_Data : IO.ECG_Data
+    {
+    }
+}
