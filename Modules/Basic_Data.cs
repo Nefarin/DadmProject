1fd513de1f8d8b8fd81d72cf91007472ca03c73f
aniametz <aniametz@gmail.com>
2015-12-10 15:34:27 +0100

    ECG_Data.cs and Basic_Data.cs added in Modules folder

---
+++ Modules/Basic_Data.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace EKG_Project.IO
+{
+    class Basic_Data : ECG_Data
+    {
+        private uint _frequency;
+        private uint _sampleAmount;
+        private List<Tuple<string, Vector<double>>> _signals;
+
+        public uint Frequency
+        {
+            get
+            {
+                return _frequency;
+            }
+            set
+            {
+                _frequency = value;
+            }
+        }
+
+        public uint SampleAmount
+        {
+            get
+            {
+                return _sampleAmount;
+            }
+            set
+            {
+                _sampleAmount = value;
+            }
+        }
+
+        public List<Tuple<string, Vector<double>>> Signals
+        {
+            get
+            {
+                return _signals;
+            }
+            set
+            {
+                _signals = value;
+            }
+        }
+    }
+}
