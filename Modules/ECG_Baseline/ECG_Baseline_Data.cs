81f9ed20282890407cea1acada23c8e1994b8f56
KrzysztofKaganiec <campnou@student.agh.edu.pl>
2015-12-12 00:07:45 +0100

    Data Class ECG_Baseline_Data added to store Output of this module. Module will be able to store more than one filtered signal if needed.

--- Modules/ECG_Baseline/ECG_Baseline_Data.cs
+++ Modules/ECG_Baseline/ECG_Baseline_Data.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace EKG_Project.Modules.ECG_Baseline
 {
     class ECG_Baseline_Data : IO.ECG_Data
     {
+        private List<Tuple<string, Vector<double>>> _signalsFiltered;   //taka konwencja -> jak Wam bardzo przeszkadza 
+                                                                        //mozemy pomyslec nad zmiana
+
+        public ECG_Baseline_Data() {}
+
+        public List<Tuple<string, Vector<double>>> SignalsFiltered
+        {
+            get
+            {
+                return _signalsFiltered;
+            }
+            set
+            {
+                _signalsFiltered = value;
+            }
+        }
     }
 }
