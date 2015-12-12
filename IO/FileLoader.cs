77db81eb8417b8eb8463f2eb8a4c1cdf2d0df4b5
aniametz <aniametz@gmail.com>
2015-12-12 17:34:54 +0100

    FileLoader with IECGConverter again

--- IO/FileLoader.cs
+++ IO/FileLoader.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Threading.Tasks;
+using EKG_Project.Modules;
 
 namespace EKG_Project.IO
 {
-    class FileLoader 
+    class FileLoader : IECGConverter
     {
         public FileLoader() { }
 
@@ -18,12 +19,24 @@ namespace EKG_Project.IO
                 path, extension);
         }
 
-        /*
+        public Basic_Data SaveResult() 
+        {
+            Basic_Data data = new Basic_Data();
+            return data;
+        }
+
+        public void ConvertFile(string path)
+        {
+
+        }
+
+
+        
         static void Main()
         {
             FileLoader fl = new FileLoader();
             fl.Load(@"C:\temp\2.xml");
             Console.Read();
-        }*/
+        }
     }
 }
--- IO/IECGConverter.cs
+++ IO/IECGConverter.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EKG_Project.Modules;
 
 namespace EKG_Project.IO
 {
     public interface IECGConverter
     {
+        Basic_Data SaveResult();
+        void ConvertFile(string path);
     }
 }
