d67790e9ce40a470333698ad862a7b32ca885325
aniametz <aniametz@gmail.com>
2015-12-12 17:22:12 +0100

    FileLoader.cs added

--- IO/FileLoader.cs
+++ IO/FileLoader.cs
@@ -2,12 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace EKG_Project.IO
 {
-    class FileLoader : IECGConverter
+    class FileLoader 
     {
         public FileLoader() { }
+
+        void Load(string path)
+        {
+            string extension = Path.GetExtension(path);
+            Console.WriteLine("GetExtension('{0}') returns '{1}'",
+                path, extension);
+        }
+
+        /*
+        static void Main()
+        {
+            FileLoader fl = new FileLoader();
+            fl.Load(@"C:\temp\2.xml");
+            Console.Read();
+        }*/
     }
 }
