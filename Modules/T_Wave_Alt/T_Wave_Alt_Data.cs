02c3ab608a243e3d61b27b9a382c6c3f7ba6a638
janeksuchocki <janek.suchocki@gmail.com>
2015-12-14 19:38:28 +0100

    _Data corrected

--- Modules/T_Wave_Alt/T_Wave_Alt_Data.cs
+++ Modules/T_Wave_Alt/T_Wave_Alt_Data.cs
@@ -3,24 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
 
 namespace EKG_Project.Modules.T_Wave_Alt
 {
     class T_Wave_Alt_Data : IO.ECG_Data
     {
-        private int[] _alternansIndexArray;
+        private Vector<int> _t_end_Vec;
+        private Vector<double> _ecg;
 
         public T_Wave_Alt_Data() {}
 
-        public int[] AlternansIndexArray
+        public Vector<int> t_end_Vec
         {
             get
             {
-                return _alternansIndexArray;
+                return _t_end_Vec;
             }
             set
             {
-                _alternansIndexArray = value;
+                _t_end_Vec = value;
+            }
+        }
+
+        public Vector<double> ecg
+        {
+            get
+            {
+                return _ecg;
+            }
+            set
+            {
+                _ecg = value;
             }
         }
     }
