baa2ce6c3df72bad916004840c28645a1ee5e65a
Anna <annasmo@student.agh.edu.pl>
2015-12-15 20:02:52 +0100

    documentation

--- Modules/R_Peaks/R_Peaks_Alg.cs
+++ Modules/R_Peaks/R_Peaks_Alg.cs
@@ -3,20 +3,33 @@ using System.Collections.Generic;
 using MathNet.Filtering.FIR;
 using MathNet.Numerics.LinearAlgebra;
 using EKG_Project.IO;
+using EKG_Project.Modules.ECG_Baseline;
 using System.Linq;
 
 namespace EKG_Project.Modules.R_Peaks
 {
+    #region R_Peaks Class doc
+    /// <summary>
+    /// Class that locates the R peaks in ECG 
+    /// </summary>
+    #endregion
     public partial class R_Peaks : IModule
     {
         //TO DO: parts of signal reading
-        //TO DO: swap ForLoops; documentation!
+        //TO DO: documentation!
 
         static void Main(string[] args)
         {
             //Init
             double fd = 5;
             double fg = 15;
+            List<Tuple<string, Vector<double>>> R_Peaks = new List<Tuple<string, Vector<double>>>();
+
+            //read data from ecg_baseline (TO DO!)
+            // Vector<double> sigs = ECG_Baseline_Data.SignalFiltered ;
+            //Tuple<string, Vector <double>> sig_data = sigs.Item[0];
+            //  string channel = sig_data.Item1;
+            // Vector<double> sig = sig_data.Item2;
 
             //read data from dat file
             TempInput.setInputFilePath(@"D:\biomed\DADM\C#\100v5.txt");
@@ -75,36 +88,58 @@ namespace EKG_Project.Modules.R_Peaks
                 if ((r <= sig_f.Count) && (r - window >= 0))
                 {
                     Vector<double> tempSig = pt.CutSignal(sig_f, r - window, r);
-                    locsR.Add(tempSig.MaximumIndex() + r - window + 1);
+                    locsR.Add(tempSig.MaximumIndex() + r - window + 1-10);
                 }
                 else if (r > sig_f.Count)
                 {
                     Vector<double> tempSig = pt.CutSignal(sig_f, r - window, sig_f.Count - 1);
-                    locsR.Add(tempSig.MaximumIndex() + r - window + 1);
+                    locsR.Add(tempSig.MaximumIndex() + r - window + 1-10);
                 }
                 else
                 {
                     Vector<double> tempSig = pt.CutSignal(sig_f, 0, r);
-                    locsR.Add(tempSig.MaximumIndex());
+                    locsR.Add(tempSig.MaximumIndex()-10);
                 }
 
             }
 
+
+
+            //write result to DATA
+            //Tuple<string, Vector<double>> r_data = Tuple.Create<channel, Vector<double> rPeaks>;
+            // R_Peaks.Add(r_data);
+            
             //write result to dat file
             //TempInput.setOutputFilePath(@"D:\biomed\DADM\C#\100v5R.txt");
             //TempInput.writeFile(fs, Vector<double>.Build.DenseOfArray(Rs1d));
 
             // foreach(double type in arr_ic) { Console.WriteLine(type); }
+            Console.WriteLine(pt.delay);
             Console.WriteLine();
             foreach (int loc in locsR) { Console.WriteLine(loc); }
+            Console.ReadKey();
 
         }
 
-        //PROPERTIES
+        //FIELDS
+        #region 
+        /// <summary>
+        /// Store tha value of delay in samples generates due to processing
+        /// </summary>
+        #endregion
         private uint delay { set; get; }
 
         //METHODS
-        //function that filters the signal by FIR filter (21 order)
+        #region
+        /// <summary>
+        /// Function that filters the signal by FIR filter (bandpass, 21 order)
+        /// </summary>
+        /// <param name="samplingFreq"> frequency of sampling for signal</param>
+        /// <param name="lowCutOff"> Low cut off ferquency</param>
+        /// <param name="highCutOff"> High cut off frequency</param>
+        /// <param name="rawSignal"> Signal which are going to be filtered </param>
+        /// <returns> filtered signal of ECG as a double array</returns>
+        #endregion
         public double[] Filtering(double samplingFreq, double lowCutOff, double highCutOff, double[] rawSignal)
         {
             //TO DO: add cutoffs as constants not param?
@@ -120,10 +155,15 @@ namespace EKG_Project.Modules.R_Peaks
             OnlineFirFilter filter = new OnlineFirFilter(coef);
             double[] vector_f = filter.ProcessSamples(rawSignal);
             return vector_f;
-
         }
 
-        //function that differentiate the signal (derivative filter)
+        #region
+        /// <summary>
+        /// Function that differentiate the signal (derivative filter H(z)=1/8T(-z^-2-2z^-1 +2z+2z^2))
+        /// </summary>
+        /// <param name="filteredSignal"> Filtered ECG signal</param>
+        /// <returns> derrivative signal of ECG as a double array</returns>
+        #endregion
         public double[] Derivative(double[] filteredSignal)
         {
             delay += 2;
@@ -138,7 +178,13 @@ namespace EKG_Project.Modules.R_Peaks
             return derivativeSignal;
         }
 
-        //function that squares the signal
+        #region
+        /// <summary>
+        /// Function that squares the signal 
+        /// </summary>
+        /// <param name="derivativeSignal"> derivative signal of ECG returned by Derivative method</param>
+        /// <returns> Squared values of derivative of ECG as a double array </returns>
+        #endregion
         public double[] Squaring(double[] derivativeSignal)
         {
             double[] squaredSignal = new double[derivativeSignal.Length];
@@ -149,7 +195,14 @@ namespace EKG_Project.Modules.R_Peaks
             return squaredSignal;
         }
 
-        //function that integrates the signal in moving-window
+        #region
+        /// <summary>
+        /// Function that integrates the signal in moving-window (width equals 150 ms)
+        /// </summary>
+        /// <param name="squaredSignal"> squared signal returnet by Squaring method</param>
+        /// <param name="fs"> sampling frequency of anlysed signal</param>
+        /// <returns> integration of signal as a double array </returns>
+        #endregion
         public double[] Integrating(double[] squaredSignal, uint fs)
         {
             double[] integratedSignal = new double[squaredSignal.Length];
@@ -165,7 +218,15 @@ namespace EKG_Project.Modules.R_Peaks
             return integratedSignal;
         }
 
-        //function that detects peaks in signal which are located in determined distance from each other
+        #region
+        /// <summary>
+        /// Function that detects peaks (local maxima) in signal which are located in determined distance from each other
+        /// </summary>
+        /// <param name="signal"> Analysed signal with local maxima</param>
+        /// <param name="fs"> sampling  grequency of the signal</param>
+        /// <param name="distanceInSec"> Distance in seconds - minimum distance between next peaks</param>
+        /// <returns> int array which contains teh localisation of peaks in signal </returns>
+        #endregion
         int[] FindPeaks(double[] signal, uint fs, double distanceInSec)
         //TO DO: distance--> threshold???
         {
@@ -193,7 +254,15 @@ namespace EKG_Project.Modules.R_Peaks
             return potRs.ToArray();
         }
 
-        //function that choose some fragment of the input signal(begin i end implicit)
+        #region
+        /// <summary>
+        /// Function that choose some fragment of the input signal(between begin and end)
+        /// </summary>
+        /// <param name="inputSignal"> The signal form which we cut its fragment </param>
+        /// <param name="begin"> the begining sample  of signal (implicit)</param>
+        /// <param name="end"> the last sample of signal (implicit)</param>
+        /// <returns> cutted signal from teh whole signal as Vector of double </returns>
+        #endregion
         Vector<double> CutSignal(Vector<double> inputSignal, int begin, int end)
         {
             int len = end - begin + 1;
