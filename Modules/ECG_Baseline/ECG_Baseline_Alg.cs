4af009284b355486bd68a2ad5636478486f0aa56
Rakso91 <cabala@student.agh.edu.pl>
2015-12-15 13:53:06 +0100

    Savitzky-Golay Filter almost finished

--- Modules/ECG_Baseline/ECG_Baseline_Alg.cs
+++ Modules/ECG_Baseline/ECG_Baseline_Alg.cs
@@ -1,4 +1,7 @@
 ﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearRegression;
 
 namespace EKG_Project.Modules.ECG_Baseline
 {
@@ -8,6 +11,70 @@ namespace EKG_Project.Modules.ECG_Baseline
 
         public class Filter
         {
+
+            public Vector<double> savitzky_golay(Vector<double> signal, int window_size, int type)
+            {
+
+                int signal_size = signal.Count;
+                if ((window_size % 2) == 0)
+                {
+                    window_size = window_size + 1;
+                }
+
+                Vector<double> signal_extension_front = Vector<double>.Build.Dense((window_size / 2), signal[0]);
+                Vector<double> signal_extension_back = Vector<double>.Build.Dense((window_size / 2), signal[signal_size - 1]);
+
+                int signal_extension_size = signal_extension_front.Count;
+
+                Vector<double> signal_extended = Vector<double>.Build.Dense(2*signal_extension_size + signal_size, 0); //przygotowanie zmiennej, w której znajdzie się sygnał przygotowany do filtracji
+                Vector<double> signal_filtered = Vector<double>.Build.Dense(signal_size, 0); //zmienna na sygnał przefiltrowany
+
+                for (int i = 0; i < signal_extension_size; i++)
+                {
+                    signal_extended[i] = signal_extension_front[i];                   //powielenie piewszej próbki sygnału wejściowego
+                }
+                for (int i = 0; i < signal_size; i++)
+                {
+                    signal_extended[i + signal_extension_size] = signal[i];                   //powielenie piewszej próbki sygnału wejściowego
+                }
+                for (int i = 0; i < signal_extension_size; i++)
+                {
+                    signal_extended[i + signal_extension_size + signal_size] = signal_extension_back[i];                   //powielenie piewszej próbki sygnału wejściowego
+                }
+                Vector<double> samples = Vector<double>.Build.Dense(signal_extended.Count, 0);
+
+                for (int i = 0; i < samples.Count; i++)
+                {
+                    samples[i] = i + 1;
+                }
+
+                double[] coeff = new double[3];
+                Vector<double> samples_destination = Vector<double>.Build.Dense(window_size, 0);
+                Vector<double> signal_extended_destination = Vector<double>.Build.Dense(window_size, 0);
+
+                for (int i = 0; i < signal_size; i++)
+                {
+                    samples.CopySubVectorTo(samples_destination, i, 0, window_size);
+                    signal_extended.CopySubVectorTo(signal_extended_destination, i, 0, window_size);
+
+                    coeff = Fit.Polynomial(samples_destination.ToArray(), signal_extended_destination.ToArray(), 3, DirectRegressionMethod.NormalEquations);
+                }
+
+                System.Console.WriteLine(signal_extension_front.ToString());
+                System.Console.WriteLine("===========================================");
+                System.Console.WriteLine(signal_extension_back.ToString());
+                System.Console.WriteLine("===========================================");
+                System.Console.WriteLine(window_size);
+                System.Console.WriteLine("===========================================");
+                System.Console.WriteLine(signal_extended.ToString());
+                System.Console.WriteLine("===========================================");
+                System.Console.WriteLine(coeff[0]);
+
+
+
+                return Vector<double>.Build.Random(10);
+            }
+
             public Vector<double> moving_average(Vector<double> signal, int window_size)
             {
                 int signal_size = signal.Count; //rozmiar sygału wejściowego
@@ -47,16 +114,12 @@ namespace EKG_Project.Modules.ECG_Baseline
          
             Vector<double> signal = Vector<double>.Build.DenseOfArray(input_signal);
             int signal_size = signal.Count;
-            int window_size = 3;
+            int window_size = 10;
             Vector<double> signal_filtered = Vector<double>.Build.Dense(signal_size, 0);
 
             Filter newFilter = new Filter();
-            signal_filtered = newFilter.moving_average(signal, window_size);
+            signal_filtered = newFilter.savitzky_golay(signal, window_size, 1);
 
-
-            System.Console.WriteLine("===========================================================================");
-            System.Console.WriteLine(signal_filtered.ToString());
-            System.Console.WriteLine("===========================================================================");
             System.Console.WriteLine("Press any key to exit.");
             System.Console.ReadKey();
         }
