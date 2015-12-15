e7dcbb2b8571dfd53cdeb4898f9729fb8aac28c4
Zaeus <wpb1992@gmail.com>
2015-12-15 16:48:03 +0100

    Przekaz kody - wersja jeszcze nie działająca!

--- Modules/Heart_Class/Heart_Class_Alg.cs
+++ Modules/Heart_Class/Heart_Class_Alg.cs
@@ -15,229 +15,177 @@ namespace EKG_Project.Modules.Heart_Class
 {
     public partial class Heart_Class : IModule
     {
+        private Heart_Class_Data HeartClassData = new Heart_Class_Data();
+
+        #region Documentation
+        /// <summary>
+        /// Test method of Heart_Class module
+        /// </summary>
+        #endregion
         public static void Main()
-        {  
+        {
+            Heart_Class HeartClass = new Heart_Class();
             TempInput.setInputFilePath(@"C:\Users\Kamillo\Desktop\Kasia\DADM proj\signal.txt");
             uint fs = TempInput.getFrequency();
-            Vector<double> sig = TempInput.getSignal();
-
+            HeartClass.HeartClassData.Signal = TempInput.getSignal();
             TempInput.setInputFilePath(@"C:\Users\Kamillo\Desktop\Kasia\DADM proj\qrsOnset.txt");
-            Vector<double> qrsOnset = TempInput.getSignal();
+            HeartClass.HeartClassData.QrsOnset = TempInput.getSignal();
             TempInput.setInputFilePath(@"C:\Users\Kamillo\Desktop\Kasia\DADM proj\qrsEnd.txt");
-            Vector<double> qrsEnd = TempInput.getSignal();
+            HeartClass.HeartClassData.QrsEnd = TempInput.getSignal();
+            TempInput.setInputFilePath(@"C:\Users\Kamillo\Desktop\Kasia\DADM proj\qrsEnd.txt");
+            // uwaga tu mam pozniej wrzucic plik qrsR.txt
+            HeartClass.HeartClassData.QrsR = TempInput.getSignal();
+            HeartClass.SetQrsComplex(); // to nie powinna być osobna klasa ale metoda w klasie Heart_Class
 
-            TempInput.setInputFilePath(@"C:\Users\Kamillo\Desktop\Kasia\DADM proj\qrsEnd.txt"); // uwaga tu mam pozniej wrzucic plik qrsR.txt
-            Vector<double> qrsR = TempInput.getSignal();
-
-
-            QrsVectors newQrsVectors = new QrsVectors();
-
-            newQrsVectors.Signal = sig;
-            newQrsVectors.QrsOnset = qrsOnset;
-            newQrsVectors.QrsEnd = qrsEnd;
-            newQrsVectors.QrsR = qrsR;
-            newQrsVectors.SetQrsComplex();
-            
-
-           
-
-
-   
             TempInput.setOutputFilePath(@"C:\Users\Kamillo\Desktop\Kasia\DADM proj\out_sig.txt");
-            TempInput.writeFile(fs, sig);
+            TempInput.writeFile(fs, HeartClass.HeartClassData.Signal);
             TempInput.setOutputFilePath(@"C:\Users\Kamillo\Desktop\Kasia\DADM proj\out_on.txt");
-            TempInput.writeFile(fs, qrsOnset);
+            TempInput.writeFile(fs, HeartClass.HeartClassData.QrsOnset);
             //TempInput.setOutputFilePath(@"C:\Users\Kamillo\Desktop\Kasia\DADM proj\matrix.txt");
             //TempInput.writeFileM(fs, qrsMatrix);
-
         }
 
-
-        public class QrsVectors
+        #region Documentation
+        /// <summary>
+        /// TODO
+        /// </summary>
+        #endregion
+        private void SetQrsComplex()
         {
-            private Vector<double> _signal;
-            private Vector<double> _qrsOnset; // docelowo int
-            private Vector<double> _qrsEnd;   // docelowo int
-            private int _qrsNumber;
-            private Vector<double> _qrsR;   // docelowo int
-
-            private Vector<double> _singleQrs = Vector<double>.Build.Dense(1);
-            private List<Tuple<int, Vector<double>>> _QrsComplex = new List<Tuple<int, Vector<double>>>();
-
-            public Vector<double> Signal
+            for (int i = 0; i < HeartClassData.QrsNumber; i++)
             {
-                get { return _signal; }
-                set { _signal = value; }
+                double singleQrsOnset = HeartClassData.QrsOnset.At(i);
+                double signleQrsEnd = HeartClassData.QrsEnd.At(i);
+                int qrsLength = (int)(signleQrsEnd - singleQrsOnset+1);
+                HeartClassData.SingleQrs = Vector<double>.Build.Dense(qrsLength);
+                int singleQrsR = (int)HeartClassData.QrsR.At(i);
+                HeartClassData.Signal.CopySubVectorTo(HeartClassData.SingleQrs, sourceIndex: (int)singleQrsOnset, targetIndex: 0, count: qrsLength);
+                Tuple<int, Vector<double>> a = new Tuple<int, Vector<double>>(singleQrsR, HeartClassData.SingleQrs);
+                HeartClassData.QrsComplex.Add(a);
             }
+        }
+        // TODO get QrsComplex
+    }
 
-            public Vector<double> QrsOnset
+    public class QrsComplex
+    {
+        static Vector<double> qrssignal;
+        double malinowskaCoefficient;
+        double pnRatio;
+        double speedAmpltudeRatio;
+        double fastSample;
+        double qRSTime;
+        int qrsLength = qrssignal.Count();
+
+        void CountMalinowskaFactor(Vector<double> qrssignal, uint fs)
+        {
+            double surface = Integrate(qrssignal);
+            double perimeter = Perimeter(qrssignal, fs);
+
+            if (perimeter != 0)
             {
-                get { return _qrsOnset; }
-                set
-                {
-                    //powinien byc typ int! ale to pozniej, bo klasa TempInut nie wczytuje int
-                    _qrsOnset = value;
-                    _qrsNumber = _qrsOnset.Count();
-                }
+                malinowskaCoefficient = surface / perimeter;
             }
-
-            public Vector<double> QrsEnd
-            {
-                get { return _qrsEnd; }
-                set { _qrsEnd = value; }
-            }
-
-            public Vector<double> QrsR
-            {
-                get { return _qrsR; }
-                set { _qrsR = value; }
-            }
-
-            public void SetQrsComplex()
-            {
-                
-                for (int i = 0; i < _qrsNumber; i++)
-                {
-                    
-                    double singleQrsOnset = _qrsOnset.At(i);
-                    double signleQrsEnd = _qrsEnd.At(i);
-                    int qrsLength = (int)(signleQrsEnd - singleQrsOnset+1);
-                    _singleQrs = Vector<double>.Build.Dense(qrsLength);
-                    int singleQrsR = (int) _qrsR.At(i);
-
-                    _signal.CopySubVectorTo(_singleQrs, sourceIndex: (int)singleQrsOnset, targetIndex: 0, count: qrsLength);
-                    Tuple<int, Vector<double>> a = new Tuple<int, Vector<double>>(singleQrsR, _singleQrs);
-                    _QrsComplex.Add(a);
-
-                }
-            } 
-
-            // TODO get QrsComplex
+            else malinowskaCoefficient = 0;
         }
 
-
-
-        public class QrsComplex
+        double Integrate(Vector<double> qrssignal)
         {
-            static Vector<double> qrssignal;
-            double malinowskaCoefficient;
-            double pnRatio;
-            double speedAmpltudeRatio;
-            double fastSample;
-            double qRSTime;
-            int qrsLength = qrssignal.Count();
 
-            void CountMalinowskaFactor(Vector<double> qrssignal, uint fs)
+            double result = 0;
+            foreach (double value in qrssignal)
             {
-                double surface = Integrate(qrssignal);
-                double perimeter = Perimeter(qrssignal, fs);
-
-                if (perimeter != 0)
-                {
-                    malinowskaCoefficient = surface / perimeter;
-                }
-                else malinowskaCoefficient = 0;
-            }
-
-            double Integrate(Vector<double> qrssignal)
-            {
-
-                double result = 0;
-                foreach (double value in qrssignal)
-                {
-                    if (value < 0)
-                        result = result - value;
-                    else
-                        result = result + value;
-
-                }
-                return result;
-            }
-
-            double Perimeter(Vector<double> qrssignal, uint fs)
-            {
-                double timeBtw2points = 1 / fs;
-                double result = 0;
-                double a, b;
-
-                for (int i = 0; i < (qrsLength - 1); i++)
-                {
-                    a = qrssignal.At(i);
-                    b = qrssignal.At(i + 1);
-                    result = result + Math.Sqrt((timeBtw2points * timeBtw2points) + ((b - a) * (b - a)));
-                }
-                return result;
-            }
-
-            double PnRatio(Vector<double> qrssignal)
-            {
-                double result = 0;
-                double positiveAmplitude = 0;
-                double negativeAmplitude = 0;
-
-                foreach (double value in qrssignal)
-                {
-                    if (value < 0)
-                        negativeAmplitude = negativeAmplitude + (-value);
-                    else
-                        positiveAmplitude = positiveAmplitude + value;
-
-                }
-                if (negativeAmplitude == 0)
-                    result = positiveAmplitude;
+                if (value < 0)
+                    result = result - value;
                 else
-                    result = positiveAmplitude / negativeAmplitude;
-                return result;
+                    result = result + value;
             }
+            return result;
+        }
 
-            double SpeedAmpRatio(Vector<double> qrssignal)
+        double Perimeter(Vector<double> qrssignal, uint fs)
+        {
+            double timeBtw2points = 1 / fs;
+            double result = 0;
+            double a, b;
+
+            for (int i = 0; i < (qrsLength - 1); i++)
             {
-                double[] speed;
-                speed = new double[qrsLength - 2];
-                double maxSpeed;
-                double maxAmp;
-
-                for (int i = 0; i < (qrsLength - 2); i++)
-                {
-                    speed[i] = Math.Abs(qrssignal.At(i + 2) + qrssignal.At(i) - 2 * qrssignal.At(i + 1));
-                }
-                maxSpeed = speed.Max();
-                maxAmp = Math.Abs(qrssignal.Maximum() - qrssignal.Minimum());
-                return maxSpeed / maxAmp;
+                a = qrssignal.At(i);
+                b = qrssignal.At(i + 1);
+                result = result + Math.Sqrt((timeBtw2points * timeBtw2points) + ((b - a) * (b - a)));
             }
+            return result;
+        }
 
-            double FastSampleCount(Vector<double> qrssignal)
+        double PnRatio(Vector<double> qrssignal)
+        {
+            double result = 0;
+            double positiveAmplitude = 0;
+            double negativeAmplitude = 0;
+
+            foreach (double value in qrssignal)
             {
-                double[] speed;
-                speed = new double[qrsLength - 1];
-                double threshold;
-                int counter = 0;
-                int speedLength;
+                if (value < 0)
+                    negativeAmplitude = negativeAmplitude + (-value);
+                else
+                    positiveAmplitude = positiveAmplitude + value;
 
-                for (int i = 0; i < (qrsLength - 1); i++)
-                {
-                    speed[i] = qrssignal.At(i + 1) - qrssignal.At(i);
-                }
-                threshold = 0.4 * speed.Max();
-
-                foreach (double value in speed)
-                {
-                    if (value >= threshold)
-                        counter = counter + 1;
-                }
-
-                speedLength = speed.GetLength(0);
-                return counter / speedLength;
             }
+            if (negativeAmplitude == 0)
+                result = positiveAmplitude;
+            else
+                result = positiveAmplitude / negativeAmplitude;
+            return result;
+        }
 
-            double QrsDuration(Vector<double> qrssignal, uint fs)
+        double SpeedAmpRatio(Vector<double> qrssignal)
+        {
+            double[] speed;
+            speed = new double[qrsLength - 2];
+            double maxSpeed;
+            double maxAmp;
+
+            for (int i = 0; i < (qrsLength - 2); i++)
             {
-                double samplingInterval = 1 / fs;
-                return qrsLength * samplingInterval;
-
+                speed[i] = Math.Abs(qrssignal.At(i + 2) + qrssignal.At(i) - 2 * qrssignal.At(i + 1));
             }
+            maxSpeed = speed.Max();
+            maxAmp = Math.Abs(qrssignal.Maximum() - qrssignal.Minimum());
+            return maxSpeed / maxAmp;
+        }
+
+        double FastSampleCount(Vector<double> qrssignal)
+        {
+            double[] speed;
+            speed = new double[qrsLength - 1];
+            double threshold;
+            int counter = 0;
+            int speedLength;
+
+            for (int i = 0; i < (qrsLength - 1); i++)
+            {
+                speed[i] = qrssignal.At(i + 1) - qrssignal.At(i);
+            }
+            threshold = 0.4 * speed.Max();
+
+            foreach (double value in speed)
+            {
+                if (value >= threshold)
+                    counter = counter + 1;
+            }
+
+            speedLength = speed.GetLength(0);
+            return counter / speedLength;
+        }
+
+        double QrsDuration(Vector<double> qrssignal, uint fs)
+        {
+            double samplingInterval = 1 / fs;
+            return qrsLength * samplingInterval;
 
         }
 
     }
+
 }
--- Modules/Heart_Class/Heart_Class_Data.cs
+++ Modules/Heart_Class/Heart_Class_Data.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using MathNet.Numerics.LinearAlgebra;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,105 @@ namespace EKG_Project.Modules.Heart_Class
 {
     class Heart_Class_Data : IO.ECG_Data
     {
+        private Vector<double> _signal;          // inicjalizacja przez wczytanie Vector z pliku
+        private Vector<double> _qrsOnset;        // inicjalizacja przez wczytanie Vector z pliku
+        private Vector<double> _qrsEnd;          // inicjalizacja przez wczytanie Vector z pliku
+        private int _qrsNumber;                  // inicjalizacja przez zliczenie elementów _qrsOnset
+        private Vector<double> _qrsR;            // inicjalizacja przez wczytanie Vector z pliku
+        private Vector<double> _singleQrs;       // inicjalizacja w kontruktorze
+        private List<Tuple<int, Vector<double>>> _QrsComplex; // inicjalizacja w kontruktorze
+
+        #region Documentation
+        /// <summary>
+        /// TODO
+        /// </summary>
+        #endregion
+        public Vector<double> Signal
+        {
+            get { return _signal; }
+            set { _signal = value; }
+        }
+
+        #region Documentation
+        /// <summary>
+        /// TODO
+        /// </summary>
+        #endregion
+        public Vector<double> QrsOnset
+        {
+            get { return _qrsOnset; }
+            set
+            {
+                //powinien byc typ int! ale to pozniej, bo klasa TempInut nie wczytuje int
+                _qrsOnset = value;
+                QrsNumber = _qrsOnset.Count();
+            }
+        }
+
+        #region Documentation
+        /// <summary>
+        /// TODO
+        /// </summary>
+        #endregion
+        public Vector<double> QrsEnd
+        {
+            get { return _qrsEnd; }
+            set { _qrsEnd = value; }
+        }
+
+        #region Documentation
+        /// <summary>
+        /// TODO
+        /// </summary>
+        #endregion
+        public Vector<double> QrsR
+        {
+            get { return _qrsR; }
+            set { _qrsR = value; }
+        }
+
+        #region Documentation
+        /// <summary>
+        /// TODO
+        /// </summary>
+        #endregion
+        public int QrsNumber
+        {
+            get { return _qrsNumber; }
+            set { _qrsNumber = value; }
+        }
+
+        #region Documentation
+        /// <summary>
+        /// TODO
+        /// </summary>
+        #endregion
+        public Vector<double> SingleQrs
+        {
+            get { return _singleQrs; }
+            set { _singleQrs = value; }
+        }
+
+        #region Documentation
+        /// <summary>
+        /// TODO
+        /// </summary>
+        #endregion
+        public List<Tuple<int, Vector<double>>> QrsComplex
+        {
+            get { return _QrsComplex; }
+            set { _QrsComplex = value; }
+        }
+
+        #region Documentation
+        /// <summary>
+        /// Not parameterized constructor of Heart_Class_Data.cs data class
+        /// </summary>
+        #endregion
+        public Heart_Class_Data()
+        {
+            _QrsComplex = new List<Tuple<int, Vector<double>>>();
+            _singleQrs = Vector<double>.Build.Dense(1);
+        }
     }
 }
