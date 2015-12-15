d2cbc06db16317853f5ef24c7bf7f845760256ab
Paulina Soltys <soltys221@gmail.com>
2015-12-15 18:47:13 +0100

    Data conversion, yeah

--- Modules/HRV_DFA/HRV_DFA_Alg.cs
+++ Modules/HRV_DFA/HRV_DFA_Alg.cs
@@ -3,19 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MathNet.Numerics;
+using EKG_Project.IO;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace EKG_Project.Modules.HRV_DFA
 {
     public partial class HRV_DFA : IModule
     {
-        //To DO :
-        // Methods needed : all :(
 
         public static void Main(string[] args)             
         {
-            //Test Main 2
+            //read data from file
+            TempInput.setInputFilePath(@"C:\Users\Paulina\Desktop\DADM\R_100.txt");
+            uint fs = TempInput.getFrequency();
+            Vector<double> sig = TempInput.getSignal();
+
+            HRV_DFA hd = new HRV_DFA();
+
+            //samples to time convertion [ms]
+            Vector<double> tacho_rr = hd.TimeConvert(fs, sig.ToArray());  
+           
+
+            Console.WriteLine(sig);
+            Console.WriteLine(fs);
+            Console.WriteLine(tacho_rr);
+            Console.ReadKey();
 
         }
+
+        // METHODS
+        // function that converts samples to time [ms]
+        public Vector<double> TimeConvert(uint samplFreq, double[] rRawSamples)
+        {
+            int signal_size = rRawSamples.Count();
+            Vector < double > tachos_r = Vector<double>.Build.Dense(signal_size);
+
+            for (int i = 0; i < signal_size; i++)
+            {
+                tachos_r[i] = rRawSamples[i] * 1000/samplFreq ;     // [ms]
+            }
+
+            return tachos_r;
+        }
+
     }
 }
--- Modules/HRV_DFA/HRV_DFA_Data.cs
+++ Modules/HRV_DFA/HRV_DFA_Data.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace EKG_Project.Modules.HRV_DFA
 {
     class HRV_DFA_Data : IO.ECG_Data
     {
-    }
+        //przygotować plik Data 
+        /*private List<Tuple<string, Vector<double>>> _signalsFiltered;   //taka konwencja -> jak Wam bardzo przeszkadza 
+                                                                        //mozemy pomyslec nad zmiana
+        public ECG_Baseline_Data() { }
+        public List<Tuple<string, Vector<double>>> SignalsFiltered
+        {
+            get
+            {
+                return _signalsFiltered;
+            }
+            set
+            {
+                _signalsFiltered = value;
+            }*/
+        }
 }
