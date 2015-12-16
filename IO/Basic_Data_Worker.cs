9a47c0052a28f5603f17e72caedbbaadb592fa2b
aniametz <aniametz@gmail.com>
2015-12-16 23:27:01 +0100

    Empty workers added

---
+++ IO/Basic_Data_Worker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EKG_Project.Modules;
+
+namespace EKG_Project.IO
+{
+    public class Basic_Data_Worker : IECG_Worker
+    {
+        public Basic_Data_Worker() { }
+
+        public void Save(ECG_Data data)
+        {
+
+        }
+        public void Load()
+        {
+
+        }
+    }
+}
---
+++ IO/ECG_Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EKG_Project.Modules;
+
+namespace EKG_Project.IO
+{
+    public class ECG_Worker 
+    {
+        IECG_Worker worker;
+
+        public ECG_Worker() { }
+    }
+}
