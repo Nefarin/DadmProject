b9e9bb49a54b440416b7adb9123d8e782275a978
MK <zxdsl852@gmail.com>
2015-12-17 09:18:16 +0100

    xD

--- Modules/HRV1/HRV1_Data.cs
+++ Modules/HRV1/HRV1_Data.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace EKG_Project.Modules.HRV1
 {
