6b5a606196b992e0c6c429d5283b90824da5845b
Ewelaa <eweliblicharz@gmail.com>
2015-12-17 14:07:37 +0100

    data&params_HRV2

--- Modules/HRV2/HRV2_Data.cs
+++ Modules/HRV2/HRV2_Data.cs
@@ -8,5 +8,88 @@ namespace EKG_Project.Modules.HRV2
 {
     class HRV2_Data : IO.ECG_Data
     {
+        private double _tinn;
+        public double Tinn
+        {
+            get
+            {
+                return _tinn;
+            }
+
+            set
+            {
+                _tinn = value;
+            }
+        }
+
+        private double _triangleIndex;
+        public double TriangleIndex
+        {
+            get
+            {
+                return _triangleIndex;
+            }
+
+            set
+            {
+                _triangleIndex = value;
+            }
+        }
+
+        private double _sd1;
+        public double SD1
+        {
+            get
+            {
+                return _sd1;
+            }
+
+            set
+            {
+                _sd1 = value;
+            }
+        }
+
+        private double _sd2;
+        public double SD2
+        {
+            get
+            {
+                return _sd2;
+            }
+
+            set
+            {
+                _sd2 = value;
+            }
+        }
+
+        private List<double> _histogramData;
+        public List<double> HistogramData
+        {
+            get
+            {
+                return _histogramData;
+            }
+
+            set
+            {
+                _histogramData = value;
+            }
+        }
+
+        private List<double> _poincarePlotData;
+        public List<double> PoincarePlotData
+        {
+            get
+            {
+                return _poincarePlotData;
+            }
+
+            set
+            {
+                _poincarePlotData = value;
+            }
+        }
     }
 }
