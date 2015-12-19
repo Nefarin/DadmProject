cb9f9207e2acc62b09339088552ed3b3d0bc654a
Agnieszka Kajda <agnkajda@student.agh.edu.pl>
2015-12-19 19:36:24 +0100

    heart axis data

--- Modules/Heart_Axis/Heart_Axis_Data.cs
+++ Modules/Heart_Axis/Heart_Axis_Data.cs
@@ -8,5 +8,14 @@ namespace EKG_Project.Modules.Heart_Axis
 {
     class Heart_Axis_Data : ECG_Data
     {
+        //Heart_Axis Module output parameters
+        private double _heartAxis;
+
+        public double HeartAxis { get; set; }
+
+        public Heart_Axis_Params(double heartAxis)
+        {
+            _heartAxis = heartAxis;
+        }
     }
 }
