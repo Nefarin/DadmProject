cfc3b65523c0fbd874f4fbe6bc7688387323ccf4
wodzinsk@student.agh.edu.pl <nefrius@gmail.com>
2016-01-04 10:51:58 +0100

    Commented some code.. This is not C++ -.-

--- Modules/ST_Segment/ST_Segment_Data.cs
+++ Modules/ST_Segment/ST_Segment_Data.cs
@@ -1,67 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MathNet.Numerics.LinearAlgebra;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using MathNet.Numerics.LinearAlgebra;
 
 
-namespace EKG_Project.Modules.ST_Segment
-{
-    class ST_Segment_Data : ECG_Data
-    {
-        private uint _frequency;
-        private uint _dJ;
-        private uint _dST; // czy musze definowac wszystkie zeminne ktorych uzyje ja a nie gui ?
+//namespace EKG_Project.Modules.ST_Segment
+//{
+//    class ST_Segment_Data : ECG_Data
+//    {
+//        private uint _frequency;
+//        private uint _dJ;
+//        private uint _dST; // czy musze definowac wszystkie zeminne ktorych uzyje ja a nie gui ?
 
 
-        private List<Tuple<string, Vector<double>>> _J;   /// Wektor początków odcinka ST,położenia J (nr próbki i jej wartość)
-        private List<Tuple<string, Vector<double>>> _ST;     /// Wektor końców odcinka ST,położenia ST (nr próbki i jej wartość)
-            // nie powinno być List<Tuple<double,double>>> _J; ? bo nie jestem pewna
+//        private List<Tuple<string, Vector<double>>> _J;   /// Wektor początków odcinka ST,położenia J (nr próbki i jej wartość)
+//        private List<Tuple<string, Vector<double>>> _ST;     /// Wektor końców odcinka ST,położenia ST (nr próbki i jej wartość)
+//            // nie powinno być List<Tuple<double,double>>> _J; ? bo nie jestem pewna
 
-        private string>>> _diagnoza;    /// zwróci informacje tekstową o jednym z 6 szablonów odcinka (normlany, obniżenie równoległe, obniżenie skośne, obniżenie miseczkowate, uniesienie wklęsłe, uniesienie wypukłe
+//        private string>>> _diagnoza;    /// zwróci informacje tekstową o jednym z 6 szablonów odcinka (normlany, obniżenie równoległe, obniżenie skośne, obniżenie miseczkowate, uniesienie wklęsłe, uniesienie wypukłe
         
-            public ST_Segment_Data() { }
+//            public ST_Segment_Data() { }
 
-        public List<Tuple<string, Vector<double>>> J
-        {
-            get
-            {
-                return _J;
-            }
-            set
-            {
-                _J = value;
-            }
-        }
-
-
-        public List<Tuple<string, Vector<double>>> ST
-        {
-            get
-            {
-                return _ST;
-            }
-            set
-            {
-                _ST = value;
-            }
-        }
+//        public List<Tuple<string, Vector<double>>> J
+//        {
+//            get
+//            {
+//                return _J;
+//            }
+//            set
+//            {
+//                _J = value;
+//            }
+//        }
 
 
-        public List<Tuple<string, Vector<double>>> diagnoza   //tu raczej nie powinno być List<Tuple ?
-        {
-            get
-            {
-                return _diagnoza;
-            }
-            set
-            {
-                _diagnoza = value;
-            }
-        }
+//        public List<Tuple<string, Vector<double>>> ST
+//        {
+//            get
+//            {
+//                return _ST;
+//            }
+//            set
+//            {
+//                _ST = value;
+//            }
+//        }
 
-    }
+
+//        public List<Tuple<string, Vector<double>>> diagnoza   //tu raczej nie powinno być List<Tuple ?
+//        {
+//            get
+//            {
+//                return _diagnoza;
+//            }
+//            set
+//            {
+//                _diagnoza = value;
+//            }
+//        }
+
+//    }
 
 
 
--- Modules/Sleep_Apnea/Sleep_Apnea_Data.cs
+++ Modules/Sleep_Apnea/Sleep_Apnea_Data.cs
@@ -40,33 +40,33 @@ namespace ConsoleApplication1
 		private Matrix <double> tab_res{ get; set; }
 
 		
-        public Matrix <double> sleep_apnea_plots(Vector <double> tab_R_peaks);
+   //     public Matrix <double> sleep_apnea_plots(Vector <double> tab_R_peaks);
         
-        //These matrix contatin Time and Hilbert amplitude/frequency to select it on graph
-        //Vector[0]: Time in samples
-        //Vector[1]: Hilbert amplitude
-        //Vector[2]: Hilbert freqency
+   //     //These matrix contatin Time and Hilbert amplitude/frequency to select it on graph
+   //     //Vector[0]: Time in samples
+   //     //Vector[1]: Hilbert amplitude
+   //     //Vector[2]: Hilbert freqency
 
-        public Matrix <double> sleep_apnea_output(Matrix <double> tab_R_peaks);
+   //     public Matrix <double> sleep_apnea_output(Matrix <double> tab_R_peaks);
        
-        //Matrix of pairs: begin and end sample of apnea detection
+   //     //Matrix of pairs: begin and end sample of apnea detection
 
-        public Vector <double> gui_output(Vector <double> tab_R_peaks);
-        //Vector[0]: Treshold value of min_amplitude [s]
-        //Vector[1]: Treshold value of max_frequency [Hz]
-        //Vector[2]: Apnea assessment in the time domain [%]
-        //Vector[3]: Apnea assessment in the frequency domain [%]
+   //     public Vector <double> gui_output(Vector <double> tab_R_peaks);
+   //     //Vector[0]: Treshold value of min_amplitude [s]
+   //     //Vector[1]: Treshold value of max_frequency [Hz]
+   //     //Vector[2]: Apnea assessment in the time domain [%]
+   //     //Vector[3]: Apnea assessment in the frequency domain [%]
 
-		 private Matrix <double> RR_intervals(Matrix <double> tab_R_peaks);
+		 //private Matrix <double> RR_intervals(Matrix <double> tab_R_peaks);
 		
-		 private Matrix <double> averange_filter(Matrix <double> tab_RR);
+		 //private Matrix <double> averange_filter(Matrix <double> tab_RR);
 
-		 private Matrix <double> resample(Matrix <double> tab_RR_new);	
-		 private Matrix  <double> HP_LP_filter(Matrix <double> tab_res,Matrix <double> &h_amp,Matrix <double> &h_freq);
-		 private Matrix  <double> hilbert(Matrix <double> &h_amp,Matrix <double> &h_freq);	
-		 private Matrix  <double> freq_amp_filter (Matrix <double> &h_amp,Matrix <double> &h_freq);	
-		 private Matrix  <double> median_filter (Matrix <double> &h_amp,Matrix <double> &h_freq);	 
-		 private Matrix  <double> apnea_detection (Matrix <double> tab_amp,Matrix <double> tab_freq);		
+		 //private Matrix <double> resample(Matrix <double> tab_RR_new);	
+		 //private Matrix  <double> HP_LP_filter(Matrix <double> tab_res,Matrix <double> &h_amp,Matrix <double> &h_freq);
+		 //private Matrix  <double> hilbert(Matrix <double> &h_amp,Matrix <double> &h_freq);	
+		 //private Matrix  <double> freq_amp_filter (Matrix <double> &h_amp,Matrix <double> &h_freq);	
+		 //private Matrix  <double> median_filter (Matrix <double> &h_amp,Matrix <double> &h_freq);	 
+		 //private Matrix  <double> apnea_detection (Matrix <double> tab_amp,Matrix <double> tab_freq);		
 
     }
 }
