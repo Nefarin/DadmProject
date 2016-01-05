17dc8fc8ac641064f4fc5e28fb54ff99ad731fa5
Anna <annasmo@student.agh.edu.pl>
2016-01-05 11:51:08 +0100

    New branch RPeaks - working after merging

--- Modules/R_Peaks/R_Peaks_Data.cs
+++ Modules/R_Peaks/R_Peaks_Data.cs
@@ -4,18 +4,17 @@ using MathNet.Numerics.LinearAlgebra;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MathNet.Numerics.LinearAlgebra;
 
 namespace EKG_Project.Modules.R_Peaks
 {
     class R_Peaks_Data : ECG_Data
     {
-        private List<Tuple<string, Vector<double>>> _rPeaks; 
+        private List<Tuple<string, Vector<double>>> _rPeaks;
         /// Wektor indeksów wykrytych zalamków R
-        private List<Tuple<string, Vector<double>>> _rRInterval; 
+        private List<Tuple<string, Vector<double>>> _rRInterval;
         /// Wektor odleglosci miedzy kolejnymi zalamkami R [ms]
-        
-        public R_Peaks_Data() {}
+
+        public R_Peaks_Data() { }
 
         public List<Tuple<string, Vector<double>>> RPeaks
         {
@@ -28,7 +27,7 @@ namespace EKG_Project.Modules.R_Peaks
                 _rPeaks = value;
             }
         }
-        
+
         public List<Tuple<string, Vector<double>>> RRInterval
         {
             get
