24de9cad5a5b3118a0fc2205f83253bc40f92042
Km <km>
2016-01-05 22:53:56 +0100

    interpolacja, resampling, moc sygnalu

--- Modules/Flutter/Flutter_Alg.cs
+++ Modules/Flutter/Flutter_Alg.cs
@@ -15,27 +15,137 @@ namespace EKG_Project.Modules.Flutter
         private List<int> _QRSonsets; // początek zespołu qrs
         private List<int> _Tends; // lista numerów próbek gdzie kończy się fala T
         private Vector<double> _samples; // próbki sygnału
+        private double _fs;
 
-        public Flutter(List<int> tEnds, List<int> qrsOnsets, Vector<double> samples)
+        public Flutter(List<int> tEnds, List<int> qrsOnsets, 
+            Vector<double> samples, double fs)
         {
             _Tends = tEnds;
             _QRSonsets = qrsOnsets;
             _samples = samples;
+            _fs = fs;
         }
 
         public List<Tuple<int, int>> DetectAtrialFlutter()
         {
             List<double[]> t2qrsEkgParts = GetEcgPart();
             List<double[]> spectralDensityList = CalculateSpectralDensity(t2qrsEkgParts);
+            List<double[]> frequenciesList = CalculateFrequenciesAxis(spectralDensityList);
+            TrimToGivenFreq(spectralDensityList, frequenciesList, 70.0);
+            InterpolateSpectralDensity(spectralDensityList, frequenciesList, 0.01);
+            List<double> powerList = CalculateIntegralForEachSpectrum(frequenciesList, spectralDensityList);
 
             bool[] aflDetected = new bool[spectralDensityList.Count];
 
             foreach (double[] spectralDensity in spectralDensityList)
             {
-
+                if (spectralDensity.Length > 0)
+                {                 
+                    double maxValue = spectralDensity.Max();
+                    int maxValueIndex = Array.IndexOf(spectralDensity, maxValue);
+                    
+                    
+                }
             }
 
             throw new NotImplementedException();
+        }
+
+        private List<double> CalculateIntegralForEachSpectrum(List<double[]> frequenciesList, List<double[]> spectralDensityList)
+        {
+            List<double> powerList = new List<double>(frequenciesList.Count);
+            for(int i = 0 ; i < frequenciesList.Count ; i++)
+            {
+                double sum = 0;
+                for(int j = 0 ; j < frequenciesList[i].Length-1 ; j++)
+                {
+                    sum += (spectralDensityList[i][j] + spectralDensityList[i][j + 1])
+                        * (frequenciesList[i][j + 1] - frequenciesList[i][j])
+                        * 0.5;
+                }
+                powerList.Add(sum);
+            }
+            return powerList;
+        }
+
+        private void InterpolateSpectralDensity(List<double[]> spectralDensityList, List<double[]> frequenciesList, double step)
+        {
+            for (int i = 0 ; i < spectralDensityList.Count ; i++)
+            {
+                List<double> interpolatedFreq = new List<double>((int)Math.Ceiling((frequenciesList[i].Last() - frequenciesList[i].First()) / step));
+                List<double> interpolatedSpectralDensity = new List<double>((int)Math.Ceiling((frequenciesList[i].Last() - frequenciesList[i].First()) / step));            
+                for (int j = 0; j < spectralDensityList[i].Length-1; j++)
+                {
+                    double y1 = spectralDensityList[i][j];
+                    double y2 = spectralDensityList[i][j + 1];
+                    double x1 = frequenciesList[i][j];
+                    double x2 = frequenciesList[i][j + 1];
+
+                    double a = (y2 - y1) / (x2 - x1);
+                    double b = y1 - a * x1;
+
+                    double k = x1;
+                    
+                    while(k < x2)
+                    {
+                        interpolatedFreq.Add(k);
+                        interpolatedSpectralDensity.Add(a * k + b);
+                        k += step;
+                    }
+                }
+                spectralDensityList[i] = interpolatedSpectralDensity.ToArray();
+                frequenciesList[i] = interpolatedFreq.ToArray();
+            }
+        }       
+
+        private List<double[]> CalculateFrequenciesAxis(List<double[]> spectralDensityList)
+        {
+            List<double[]> freqs = new List<double[]>();
+            foreach(var spectralDensity in spectralDensityList)
+            {
+                double[] freq = new double[spectralDensity.Length];
+                for(int i = 0 ; i < spectralDensity.Length ; i++)
+                {
+                    freq[i] = (i * _fs) / spectralDensity.Length;
+                }
+                freqs.Add(freq);
+            }
+            return freqs;
+        }
+
+        private void TrimToGivenFreq(List<double[]> spectralDensityList, List<double[]> frequenciesList, double trimFreq)
+        {
+            for (int i = 0; i < spectralDensityList.Count; i++)
+            {
+                double firstGTtrimFreq = frequenciesList[i].First(x => x > trimFreq);
+                int n = Array.IndexOf(frequenciesList[i], firstGTtrimFreq);
+                double[] trimmed = new double[n];
+                double[] freqTrimmed = new double[n];
+                for (int j = 0; j < n; j++)
+                {
+                    trimmed[j] = spectralDensityList[i][j];
+                    freqTrimmed[j] = frequenciesList[i][j];
+                }
+                spectralDensityList[i] = trimmed;
+                frequenciesList[i] = freqTrimmed;
+            }
+        }
+
+        private List<double[]> TrimSpectralDensity(List<double[]> spectralDensityList, List<double[]> frequenciesList, double trimFreq)
+        {
+            List<double[]> trimmedSpectralDensityList = new List<double[]>(spectralDensityList.Count);
+            foreach(var spectralDensity in spectralDensityList)
+            {
+                int n = (int)(trimFreq * spectralDensity.Length / _fs);
+                double[] trimmed = new double[n];
+                double[] freqTrimmed = new double[n];
+                for(int i = 0; i < n; i++)
+                {
+                    trimmed[i] = spectralDensity[i];
+                }
+                trimmedSpectralDensityList.Add(trimmed);
+            }
+            return trimmedSpectralDensityList;
         }
 
         private List<double[]> CalculateSpectralDensity(List<double[]> t2qrsEcgParts)
@@ -87,7 +197,7 @@ namespace EKG_Project.Modules.Flutter
             List<int> QRSOnsetsTmp = ReadFromCSV(Path.Combine(path, QRSOnsetsFileName)).Select(x => (int)x).ToList();
             List<int> tEndsTmp = ReadFromCSV(Path.Combine(path, TEndsFileName)).Select(x => (int)x).ToList();
 
-            Flutter flutter = new Flutter(tEndsTmp, QRSOnsetsTmp, samplesTmp);
+            Flutter flutter = new Flutter(tEndsTmp, QRSOnsetsTmp, samplesTmp, 360);
             flutter.DetectAtrialFlutter();
 
             Console.ReadKey();
