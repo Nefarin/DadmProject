f900f90ad63f3ad367b239f3b88a46366b583945
Paweł <pawcio16m@gmail.com>
2016-01-07 21:55:21 +0100

    TWave update and comments added

--- Modules/QT_Disp/QT_Disp_Alg.cs
+++ Modules/QT_Disp/QT_Disp_Alg.cs
@@ -46,13 +46,16 @@ namespace EKG_Project.Modules.QT_Disp
             QTCalculation output = new QTCalculation(test_list);
             output.setOutput();*/
 
-            /*
+            
             TempInput.setInputFilePath("D:\\Dadm_nowy\\EKG.txt");
             Vector<double> signalfile = TempInput.getSignal();
-            TWave signal = new TWave(signalfile);
+            List<Tuple<String, Vector<double>>> input = new List<Tuple<string, Vector<double>>>();
+            input.Add(Tuple.Create("My drain", signalfile));
+            
+            TWave signal = new TWave(input);
             // double[] test = { 2, -1, 3.5, -4, 4, 5, 6, -7, -8.9, 500, 10, 0, 11 };
             // Vector<double> testvec = Vector<double>.Build.DenseOfArray(test);
-            List<int> wynik = signal.ExecuteTWave();
+            List<Tuple<String,List<int>>> wynik = signal.ExecuteTWave();
             
             /*Console.WriteLine("Hello");
             double[] x= new double[10];
@@ -65,7 +68,7 @@ namespace EKG_Project.Modules.QT_Disp
             double[] wynik = MathNet.Numerics.Fit.Polynomial(x, y, 2);
             Console.WriteLine("Size" +wynik.Count());
             Console.WriteLine(wynik.ElementAt(0) +"   "+ wynik.ElementAt(1) +"      "+ wynik.ElementAt(2));*/
-            QTCalculation wyniki = new QTCalculation();
+           /* QTCalculation wyniki = new QTCalculation();
             wyniki.setOutput();
             Console.WriteLine(wyniki.meanQTInterval.ElementAt(0).Item2);
             Console.WriteLine(wyniki.meanQTInterval.ElementAt(1).Item2);
@@ -74,7 +77,7 @@ namespace EKG_Project.Modules.QT_Disp
             Console.WriteLine(wyniki.stdQTInterval.ElementAt(1).Item2);
 
             Console.WriteLine(wyniki.localQTdisp.ElementAt(0).Item2);
-            Console.WriteLine(wyniki.localQTdisp.ElementAt(1).Item2);
+            Console.WriteLine(wyniki.localQTdisp.ElementAt(1).Item2);*/
             Console.ReadKey();
 
         }
@@ -88,70 +91,72 @@ namespace EKG_Project.Modules.QT_Disp
     #endregion 
     class TWave
     {
-        //
-        String drainName;
-        T_End_Method method;
        
-        private List<int> QRS_End;
-        private List<int> P_Onset;
+        private T_End_Method method;
+        private bool alldrains;
+       
+        private List<int> QRS_End;       
         private List<int> T_EndGlobal;
        
-        public Vector<double> signal;
+        private List<Tuple<String,Vector<double>>> AllDrainSiganl;
 
         #region Documentation
         /// <summary>
         /// This constructor set fields of a class
         /// </summary>
-        /// <param name="Signal">This is a vector signal from ECG_baseline module</param>
-        /// <param name="T_EndGlobal">This list contains T_End indexes</param>
-        /// <param name="P_Onset">This list contains P_Onset indexes</param>
+        /// <param name="AllDrainSignal">This is a list of tuple which stores a drain name and a vector signal from ECG_baseline module</param>
+        /// <param name="T_EndGlobal">This list contains T_End indexes</param>      
         /// <param name="QRS_End">This list contains QRS_End indexes</param>       
-        /// <param name="drainName">This argument is a name of a drain</param>
+        /// <param name="alldrains">This argument check if the control alldrains in gui is set</param>
         /// <param name="method">This argument is a method to find T_End in a signal</param>
         #endregion
-        public TWave(Vector<double> Signal, List<int> P_Onset, List<int> QRS_End, List<int> T_EndGlobal,  String drainName, T_End_Method method)
+        public TWave(List<Tuple<String, Vector<double>>> AllDrainSiganl, List<int> QRS_End, List<int> T_EndGlobal, T_End_Method method, bool alldrains)
         {           
-            this.drainName = drainName;
+           
             this.method = method;
+            this.alldrains = alldrains;
 
-            this.signal = Signal;          
-            this.QRS_End = QRS_End;           
-            this.P_Onset = P_Onset;
+            this.AllDrainSiganl = AllDrainSiganl;          
+            this.QRS_End = QRS_End;             
             this.T_EndGlobal = T_EndGlobal;
         }
         /// <summary>
         /// Only for test
         /// </summary>
-        /// <param name="signal"></param>
-        public TWave(Vector<double> signal)
+        /// <param name="input">Signal</param>
+        public TWave(List<Tuple<String,Vector<double>>> input)
         {
             //for testes
-            this.signal = signal;
-            int[] ponset = { 295, 588,863,1151,1434,1728 };
-            int[] qrsend = { 96, 380,676,960,1244,1528 };
-            int[] tend = { 265, 553,835,1119,1410,1692 };        
+            
+           
+            int[] qrsend = { 380,676,960,1244,1528 };
+            int[] tend = { 265,553,835,1119,1410 };        
            
             this.QRS_End = qrsend.ToList();
-            this.drainName = "V2";
-            this.P_Onset = ponset.ToList();           
+            this.AllDrainSiganl = input;
+           
+                  
             this.T_EndGlobal = tend.ToList();
+            this.method = T_End_Method.PARABOLA;
+            this.alldrains = false;
         }
        
         #region Documentation
         /// <summary>
         /// This function finds T-Max indexes in signal
         /// </summary>
+        /// <param name="signal">A vector with a signal</param>
         /// <returns>List that contains T_Max indexes</returns>
         #endregion
-        private List<int> FindT_Max()
+        private List<int> FindT_Max(Vector<double> signal)
         {
-            List<int> T_MaxIndexList = new List<int>();
-            //check if we have a correct index 
-            //try to do else statements!
+            //list that stores t max indexes
+            List<int> T_MaxIndexList = new List<int>();            
+           
             if (QRS_End.ElementAt(0) < T_EndGlobal.ElementAt(0))
             {
                 //this for goes for every T_end in signal
-                for (int i = 0; i < T_EndGlobal.Count; i++)
+                for (int i = 0; i < T_EndGlobal.Count-2; i++)
                 {     
                         //check if there is not any bad recognized points            
                         if (QRS_End.ElementAt(i) != -1 && T_EndGlobal.ElementAt(i) != -1)
@@ -165,23 +170,39 @@ namespace EKG_Project.Modules.QT_Disp
                         }
                 }               
             }
+            else
+            {
+                //in case where signal started to be recorded after a QRS peak
+                for (int i = 0; i < T_EndGlobal.Count-2; i++)
+                {
+                    //check if there is not any bad recognized points            
+                    if (QRS_End.ElementAt(i) != -1 && T_EndGlobal.ElementAt(i+1) != -1)
+                    {                       
+                        T_MaxIndexList.Add((signal.SubVector(QRS_End.ElementAt(i), T_EndGlobal.ElementAt(i + 1) - QRS_End.ElementAt(i)).MaximumIndex()) + QRS_End.ElementAt(i));
+                    }
+                    else
+                    {
+                        T_MaxIndexList.Add(-1);         //insert -1 for bad-recognized points
+                    }
+                }
+            }
             return T_MaxIndexList;
         }
         #region Documentation
         /// <summary>
         /// This function finds max slope indexes in signal after T_max index
         /// </summary>
-        /// <returns>Vector that contains Max Slope indexes</returns>
+        /// <param name="signal">A vector with a signal</param>
+        /// <returns>List that contains Max Slope indexes</returns>
         #endregion
-        private List<int> Find_MaxSlope()
+        private List<int> Find_MaxSlope(Vector<double> signal)
         {
-            List<int> T_MaxIndex = FindT_Max();
-            List<int> T_MaxSlope = new List<int>();
-            
-            if (P_Onset.ElementAt(0) > T_MaxIndex.ElementAt(0))
-            {
+            List<int> T_MaxIndex = FindT_Max(signal);
+            //list that stores t_max slope need to fit a line or a parabola
+            List<int> T_MaxSlope = new List<int>();          
 
                 for (int i = 0; i < T_MaxIndex.Count; i++)
+                if(T_MaxIndex.ElementAt(i)<T_EndGlobal.ElementAt(i))
                 {
                     if (T_EndGlobal.ElementAt(i) != -1 && T_MaxIndex.ElementAt(i) != -1)
                     {                       
@@ -192,24 +213,39 @@ namespace EKG_Project.Modules.QT_Disp
                         T_MaxSlope.Add(-1);
                     }
                 }
-            }
+                else
+                {
+                    if (T_EndGlobal.ElementAt(i) != -1 && T_MaxIndex.ElementAt(i) != -1)
+                    {
+                        T_MaxSlope.Add(diff(signal.SubVector(T_MaxIndex.ElementAt(i), (T_EndGlobal.ElementAt(i+1) - T_MaxIndex.ElementAt(i)))).MinimumIndex() + T_MaxIndex.ElementAt(i));
+                    }
+                    else
+                    {
+                        T_MaxSlope.Add(-1);
+                    }
+
+                }
+           
             return T_MaxSlope;
         }
         #region Documentation
         /// <summary>
         /// This method find T_End inexes according to tangent method
         /// </summary>
-        /// <returns>Vector that contain T_End indexes</returns>
+        /// <param name="signal">A vector with a signal</param>
+        /// <returns>List that contain T_End indexes calculated according to Tangent method</returns>
         #endregion
-        private List<int> TangentMethod()
+        private List<int> TangentMethod(Vector<double> signal)
         {
+            //lsit that stores t_end indexes 
             List<int> T_End_Index = new List<int>();
             Vector<double> vectorTofit = Vector<double>.Build.Dense(9);
+            int tempindex = 0;
             double[] x = new double[9];         //variable to store x_data to fit a line
             double[] y = new double[9];         //variable to store y_data to fit a line
             Tuple<double, double> coefficiants; //variable to store output from fitting , Item1=b, Item2=a , y = ax + b
 
-            foreach (int MaxSlope in Find_MaxSlope()){
+            foreach (int MaxSlope in Find_MaxSlope(signal)){
                 if(MaxSlope != -1)
                 {                    
                     vectorTofit =  signal.SubVector(MaxSlope-4,9);
@@ -219,7 +255,9 @@ namespace EKG_Project.Modules.QT_Disp
                         x[i + 4] = MaxSlope + i;
                     }
                     coefficiants = MathNet.Numerics.Fit.Line(x, y);
-                    T_End_Index.Add((int)(-coefficiants.Item1 / coefficiants.Item2));   //finding T_End according to Tangent method
+                    tempindex = (int)(-coefficiants.Item1 / coefficiants.Item2);
+                    // here we should check if the index is correct
+                    T_End_Index.Add(tempindex);   //finding T_End according to Tangent method
 
                 }
                 else
@@ -238,12 +276,48 @@ namespace EKG_Project.Modules.QT_Disp
         /// <summary>
         /// This method find T_End indexes according to parabola method
         /// </summary>
-        /// <returns>Vector that contains T_End indexes</returns>
+        /// <param name="signal">A vector with a signal</param>
+        /// <returns>List that contains T_End indexes calculated according to parabola method</returns>
         #endregion
-        //private Vector<double> ParabolaMethod()
-        //{
+        private List<int> ParabolaMethod(Vector<double> signal)
+        {
+            List<int> T_End_Index = new List<int>();
+            List<int> T_MaxSlope = Find_MaxSlope(signal);
+            Vector<double> vectorTofit = Vector<double>.Build.Dense(6);
+            int[] step = new int[T_EndGlobal.Count];
+            double delta = new double();
+            int tempindex = -1;
 
-        //}
+            //Vector<double> vectorTofit = Vector<double>.Build.Dense(5);
+
+            double[] x = new double[7];         //variable to store x_data to fit a polynomial (parabola)
+            double[] y = new double[7];         //variable to store y_data to fit a polynomial (parabola)
+            double[] coefficients;              //variable to store output from fitting , Item1=c, Item2=b, Item3=a , y = ax^2 + bx +c
+
+            foreach (int MaxSlope in Find_MaxSlope(signal))
+            {
+                if (MaxSlope != -1)
+                {
+                    vectorTofit = signal.SubVector(MaxSlope, 7);
+                    y = vectorTofit.ToArray();
+                    for (int i = 0; i < 7; i++)
+                    {
+                        x[i] = MaxSlope + i;
+                    }
+
+                    coefficients = MathNet.Numerics.Fit.Polynomial(x, y, 2); //calculates the coeffs of parabola that fits best the slope
+                    delta = Math.Pow(coefficients[1], 2) - 4 * coefficients[2] * coefficients[0];
+                    tempindex = (int)(-coefficients[1] / (2 * coefficients[2]));
+                    //here we should check if t_end index is correct
+                    T_End_Index.Add(tempindex);   //finding T_End according to Tangent method
+                }
+                else
+                {
+                    T_End_Index.Add(-1);
+                }
+            }
+            return T_End_Index;
+        }
         #region Documentation
         /// <summary>
         /// This function calculate the difference between the next elements in vector
@@ -266,15 +340,42 @@ namespace EKG_Project.Modules.QT_Disp
         /// <summary>
         /// This function execute all methods to find a T_End indexes in a signal
         /// </summary>      
-        /// <returns>List that contains indexes of T_End according to chose method</returns>
+        /// <returns>List of tuple that contains a drain name and a list with indexes of T_End according to chose method</returns>
         #endregion
-        public List<int> ExecuteTWave()
+        public List<Tuple<String,List<int>>> ExecuteTWave()
         {
+            List<Tuple<String, List<int>>> output = new List<Tuple<string, List<int>>>();
+            //if alldrains in gui i set we calculate t_end index in all drains according to chosen method
+            if(alldrains == true)
+            {
+                for(int i = 0; i<this.AllDrainSiganl.Count(); i++)
+                {
+                    if (this.method == T_End_Method.PARABOLA)
+                    {
+                         output.Add(Tuple.Create(this.AllDrainSiganl.ElementAt(i).Item1,ParabolaMethod(this.AllDrainSiganl.ElementAt(i).Item2)));
+                    }
+                    else
+                    {
+                        output.Add(Tuple.Create(this.AllDrainSiganl.ElementAt(i).Item1, TangentMethod(this.AllDrainSiganl.ElementAt(i).Item2)));                        
+                    }
+                }
+            }
+            // if alldrains is not set we return T_End indexes detected by Waves
+            else
+            {
+                output.Add(Tuple.Create("Default", T_EndGlobal));                
+            }
+            return output;
             
-            return TangentMethod();
         }
 
     }
+
+
+
+
+
+
     
     /// <summary>
     /// This class is to calculate QT intervals statistic and QT disp from one drain and all drains
