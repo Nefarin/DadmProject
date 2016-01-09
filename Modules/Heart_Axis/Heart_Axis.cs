e109ff0fd7a32797a4489bccdad0630bdb22b8a4
Agnieszka Kajda <agnkajda@student.agh.edu.pl>
2016-01-09 03:25:09 +0100

    Interface in progress

--- Modules/Heart_Axis/Heart_Axis.cs
+++ Modules/Heart_Axis/Heart_Axis.cs
@@ -3,22 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EKG_Project.IO;
 
 namespace EKG_Project.Modules.Heart_Axis
 {
     public partial class Heart_Axis : IModule
     {
+
+        /* Zmienne do komunikacji z GUI - czy liczenie osi serca się skończyło/zostało przerwane */
+
+        private bool _ended;
+        private bool _aborted;
+
+        /* Stałe */
+
+        private const string LeadISymbol = "I";
+        private const string LeadIISymbol = "II";
+
+        /* Dane wejściowe */
+
+        //private Basic_Data_Worker _inputBasicDataWorker; //Basic_Data_Worker could not be found
+
+        /* Dane wyjściowe - Kąt osi serca  */
+
+        //private Heart_Axis_Data_Worker _output;   //Heart_Axis_Data_Worker could not be found
+        //private Heart_Axis_Data _outputData; //Heart_Axis_Data could not be found
+
         public void Abort()
         {
-            throw new NotImplementedException();
+            Aborted = true;
+            _ended = true;
         }
 
         public bool Ended()
         {
-            throw new NotImplementedException();
+            return _ended;
         }
 
-        public void Init(ModuleParams parameters)
+        public void Init(ModuleParams parameters) //moduł nie przyjmuje parametrów, co tu napisać?
         {
             throw new NotImplementedException();
         }
@@ -35,7 +57,23 @@ namespace EKG_Project.Modules.Heart_Axis
 
         public bool Runnable()
         {
+            // czy w GUI zostały ustawione parametry do obliczeń - brak parametrów -  zawsze true?
+            //return Params != null; //params does not exist
             throw new NotImplementedException();
         }
+
+        public bool Aborted
+        {
+            get
+            {
+                return _aborted;
+            }
+
+            set
+            {
+                _aborted = value;
+            }
+        }
+
     }
 }
