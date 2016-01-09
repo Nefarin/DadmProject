cb87ec8bc2e2facd8d1e310874b8f8e3fa69a330
Paulina Soltys <soltys221@gmail.com>
2016-01-09 17:38:19 +0100

    _Alg: Commit before I change my mind, linear-least-square battle

--- Modules/HRV_DFA/HRV_DFA_Alg.cs
+++ Modules/HRV_DFA/HRV_DFA_Alg.cs
@@ -4,55 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EKG_Project.IO;
+using MathNet.Numerics;
 using MathNet.Numerics.LinearAlgebra.Double;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.Statistics;
 
 namespace EKG_Project.Modules.HRV_DFA
 {
+
+
     public partial class HRV_DFA : IModule
     {
 
-        public static void Main(string[] args)             
+        public static void Main(string[] args)
         {
-            //NEEDED: MAIN: integrate, lengthN_min, lengthN_max, localTrend, fluctFn, alpha, get_n, get_Fn, get_alphas
 
             //read data from file
-            TempInput.setInputFilePath(@"C:\Users\Paulina\Desktop\DADM\R_100.txt");
+            TempInput.setInputFilePath(@"C:\Users\Paulina\Desktop\DADM\RR_100.txt");
             uint fs = TempInput.getFrequency();
             Vector<double> sig = TempInput.getSignal();
 
             HRV_DFA dfa = new HRV_DFA();
 
-            // Samples to time convertion [ms]
-            Vector<double> tacho_rr = dfa.TimeConvert(fs, sig.ToArray());
+           // Box_StepSize stepSize = new Box_StepSize;
+
+            int step = 50;
+            int start = 50;
+            int stop = 50000; 
+
+            double[] boxRanged = Generate.LinearRange(start, step, stop);
+            Vector<double> box = Vector<double>.Build.DenseOfArray(boxRanged);
 
             // samplesOrder obtaining
-            Vector<double> samplesOrder = dfa.Ordering(tacho_rr);
-
-            // Signal integration
-            Vector<double> sig_integrated = dfa.Integrate(tacho_rr);
+            Vector<double> samplesOrder = dfa.Ordering(sig);
 
             Console.WriteLine(fs);
-            Console.WriteLine(sig_integrated);
+            Console.WriteLine(box);
+            //Console.WriteLine(sig_integrated);
             Console.ReadKey();
 
         }
 
         // METHODS
-        // function that converts samples numers to time [ms]
-        public Vector<double> TimeConvert(uint samplFreq, double[] rRawSamples)
-        {
-            int signal_size = rRawSamples.Count();
-            Vector < double > tachos_r = Vector<double>.Build.Dense(signal_size);
-
-            for (int i = 0; i < signal_size; i++)
-            {
-                tachos_r[i] = rRawSamples[i] * 1000/samplFreq ;     // [ms]
-            }
-
-            return tachos_r;
-        }
 
         //function for samples ordering
         public Vector<double> Ordering(Vector<double> signal_rr)
@@ -69,20 +62,66 @@ namespace EKG_Project.Modules.HRV_DFA
         //function that integrates signal
         public Vector<double> Integrate(Vector<double> signal_rr)
         {
-            Vector<double> signal_integrated = Vector<double>.Build.Dense(signal_rr.Count(),0);
+            Vector<double> signal_integrated = Vector<double>.Build.Dense(signal_rr.Count(), 0);
 
             //Average
-            double rr_avg = signal_rr.Sum()/signal_rr.Count;
+            double rr_avg = signal_rr.Sum() / signal_rr.Count;
 
             for (int i = 0; i < signal_rr.Count - 1; i++)
             {
                 signal_integrated[0] = 0;
-                signal_integrated[i+1] = signal_rr[i] - rr_avg;  
+                signal_integrated[i + 1] = signal_rr[i] - rr_avg;
                 signal_integrated[i + 1] += signal_integrated[i];
-                signal_integrated[i+1] = Math.Abs(signal_integrated[i+1]);
+                signal_integrated[i + 1] = Math.Abs(signal_integrated[i + 1]);
             }
 
             return signal_integrated;
         }
+
+
+        public void DfaFluctuationComputation(Vector<double> dfabox, Vector<double> signal )
+        {
+            int box_length = dfabox.Count();    // number of all boxes
+            int sig_length = signal.Count();    // signal length
+
+            for(int i = 0; i < box_length; i++)
+            {
+                double boxVal = dfabox[i];
+                double box_number = sig_length / boxVal ;   // number of boxes
+                double box_qtyD = box_number * boxVal;      // quantity of samples in boxes
+                int boxValint = Convert.ToInt32(boxVal);
+                int box_qty = Convert.ToInt32(box_qtyD);
+
+               // Vector<double> yk = Vector<double>.Build.Dense(box_qty);
+                //Vector<double> yn = Vector<double>.Build.Dense(box_qty);
+
+                // Signal integration
+                Vector<double> yk = Integrate(signal);
+                Vector<double> fn = Vector<double>.Build.Dense(box_length);
+
+                for (int j = 0; j < box_number; j++)
+                {
+                    // Least-Square Fitting
+                    int ykIndex = ((j - 1) * boxValint + 1);
+                    int ykCount = j * boxValint - ykIndex;
+                    double[] x = Generate.LinearRange(1, boxValint);
+                    double[] y = yk.SubVector(ykIndex, ykCount).ToArray();
+                    double[] p = Fit.Polynomial(x, y, 1);     //fitting coefficients
+                    //Fitting curve
+                    Func<double, double> yn = Fit.PolynomialFunc(x, y, 1, MathNet.Numerics.LinearRegression.DirectRegressionMethod.NormalEquations);
+                    // dfa fluctuation F(n)
+                    //fn[i] = 
+
+
+                }
+
+
+
+            }
+            
+
+        }
+    
+
     }
 }
