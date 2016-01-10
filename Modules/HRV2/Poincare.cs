8ddd5a5dbe1ee72b46ee9b445dfbc441eb0bc0ce
ewakot <kotanskae@gmail.com>
2016-01-10 12:24:36 +0100

    poincare plot  SD1, SD2 added

--- Modules/HRV2/Poincare.cs
+++ Modules/HRV2/Poincare.cs
@@ -5,27 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using MathNet.Numerics.LinearAlgebra;
 
+
 namespace EKG_Project.Modules.HRV2
 {
     public partial class HRV2 : IModule
     {
-        void PoincarePlot( Vector<double> rr_intervals_x, Vector<double> rr_intervals_y) {
-
+        void PoincarePlot(Vector<double> rr_intervals_x, Vector<double> rr_intervals_y)
+        {
             Vector<double> RRIntervaals = InputData.RPeaks[_currentChannelIndex].Item2;
-            rr_intervals_x = RRIntervaals.Subtract( RRIntervaals[0]);
-            rr_intervals_y = RRIntervaals.Subtract( RRIntervaals.Last());
-
-            //double mean_rr_intervals; //tu bd wyliczenie średniej
-
-            //foreach (double i in RRIntervals)
-            //{
-            //    double std_power = std_power + (RRIntervaals(i) - mean_rr_intervals) * (RRIntervaals(i) - mean_rr_intervals);
-            //}
-            //double std_power2 = std_power / RRIntervaals.Length;
-            //double std = Math.Sqrt(std_power2);
-
-            //double SD1 = std(rr_intervals_x - rr_intervals_y) / Math.Sqrt(2);
-            //double SD2 = std(rr_intervals_x + rr_intervals_y) / Math.Sqrt(2);
+            rr_intervals_x = RRIntervaals.Subtract(RRIntervaals[0]);
+            rr_intervals_y = RRIntervaals.Subtract(RRIntervaals.Last());
+            
+            double SD1 = getStandardDeviation(rr_intervals_x.Subtract(rr_intervals_y)) / Math.Sqrt(2);
+            double SD2 = getStandardDeviation(rr_intervals_x.Add(rr_intervals_y)) / Math.Sqrt(2);
         }
+
+        private double getStandardDeviation(Vector<double> dataVector)
+        {
+            double average = dataVector.Average();
+            double sumOfDerivation = 0;
+            foreach (double value in dataVector)
+            {
+                sumOfDerivation += (value) * (value);
+            }
+            double sumOfDerivationAverage = sumOfDerivation / (dataVector.Count - 1);
+            return Math.Sqrt(sumOfDerivationAverage - (average * average));
+        } 
     }
-}
+    }
+    
+
+
