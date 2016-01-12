7bf3158215c20590da16f04b97caae1759136380
konradkonopka <konrad.konopka@gmail.com>
2016-01-12 18:01:28 +0100

    Move current file from old branch.

--- Modules/HRT/HRT_Params.cs
+++ Modules/HRT/HRT_Params.cs
@@ -1,12 +1,16 @@
-﻿using MathNet.Numerics.LinearAlgebra;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace EKG_Project.Modules.HRT
 {
     ///<Summary>To są parametry jakie mogę ustawić na panelu użytkownika
-   ///  i które są wczytywane do mojego modułu z GUI</Summary>
+    /// i które są wczytywane do mojego modułu z GUI</Summary>
     public class HRT_Params : ModuleParams
     {
-
-        enum RodzajWykresu { LINIOWY, SCHODKOWY};
+        //Ten moduł nie potrzebuje żadnych informacji podanych bezpośrednio z GUI
     }
 }
