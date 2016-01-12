ad72f4931bd73919affbf10496647b94719d7d79
konradkonopka <konrad.konopka@gmail.com>
2016-01-12 15:35:15 +0100

    Strange changes only to do merge with master.

--- Modules/HRT/HRT.cs
+++ Modules/HRT/HRT.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EKG_Project.IO;
+using EKG_Project.Modules.R_Peaks;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics;
 
 namespace EKG_Project.Modules.HRT
 {
     public partial class HRT : IModule
     {
+        private bool _ended;
+        private bool _aborted;
+
+        private int _currentChannelIndex;
+        private int _currentChannelLength;
+        private int _samplesProcessed;
+        private int _numberOfChannels;
+
+        private R_Peaks_Data_Worker _inputRpeaksWorker;
+
+
         public void Abort()
         {
             throw new NotImplementedException();
