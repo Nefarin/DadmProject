09766f2ceb0a12a3ff337fe1e8e4e5521e1e7da8
wodzinsk@student.agh.edu.pl <nefrius@gmail.com>
2016-01-14 23:34:08 +0100

    Stats class generated and factory added.

---
+++ Modules/Heart_Class/Heart_Class_Stats.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EKG_Project.Modules.Heart_Class
+{
+    public class Heart_Class_Stats : IModuleStats
+    {
+        public void Abort()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Aborted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Ended()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, object> GetStats()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetStatsAsString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(string analysisName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProcessStats()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Modules/IModuleStats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EKG_Project.Modules
+{
+    public interface IModuleStats
+    {
+        void Init(String analysisName);
+        void ProcessStats();
+        bool Ended();
+        bool Aborted();
+        void Abort();
+        Dictionary<String, String> GetStatsAsString();
+        Dictionary<String, Object> GetStats();
+    }
+}
