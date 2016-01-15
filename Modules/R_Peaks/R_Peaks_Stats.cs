a45b9ad03b4dcdac7e1ca87f011b7f2e2860c61d
Anna <annasmo@student.agh.edu.pl>
2016-01-15 16:17:45 +0100

    R_peaks-stats - proper

--- Modules/R_Peaks/R_Peaks_Stats.cs
+++ Modules/R_Peaks/R_Peaks_Stats.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using EKG_Project.IO;
 using MathNet.Numerics.LinearAlgebra;
+using System.Linq;
 
 namespace EKG_Project.Modules.R_Peaks
 {
@@ -64,9 +65,6 @@ namespace EKG_Project.Modules.R_Peaks
             _currentState = State.START_CHANNEL;
             _currentChannelIndex = 0;
 
-            //reading annotations 
-            //!!!! TO DO --> Tymczasowy sposób --> zmienic jesli ma to byc wczytywane automatycznie z IO  !!!!!
-            TempInput.setInputFilePath(@"D:\biomed\DADM\C#\100a.txt");
         }
 
         public void ProcessStats()
@@ -79,59 +77,40 @@ namespace EKG_Project.Modules.R_Peaks
                     break;
 
                 case (State.CALCULATE):
-                    Vector<double> currentData = _data.RPeaks[_currentChannelIndex].Item2;
-                    Vector<double> ann = TempInput.getSignal();
-                    int tp = 0;     //true positives
-                    int fp = 0;     //false positives
-                    int fn = 0;     //false negatives
-                    int total = 0;     //all proper peaks in signal
-                    int indAnn = 0;
-                    int indData = 0;
-
-                    //validation
-                    while (indAnn<ann.Count && indData < currentData.Count)
+                    Vector<double> currentDataR = _data.RPeaks[_currentChannelIndex].Item2;
+                    Vector<double> currentDataRR = _data.RRInterval[_currentChannelIndex].Item2;
+                 
+                    int noOfPeaks = currentDataR.Count;     //number of detected r_peaks
+                    double meanRR = Math.Round(currentDataRR.Sum() / currentDataRR.Count, 3);   
+                    //mean RR interval in ms//double minRR = currentDataRR.AbsoluteMinimum(); //min distance between Rs
+                    //double maxRR = currentDataRR.AbsoluteMaximum(); //max distance between Rs
+                    int noOfOverdetectedPeaks = 0;  //number of peaks to close to eachother
+                    int noOfUnderdetectedPeaks = 0; //number of peaks to far to each other
+                    foreach(var rr in currentDataRR)
                     {
-                        double d = Math.Abs(currentData[indData] - ann[indAnn]);
-                        if (d > 10)     //approved mistake = 10 samples
+                        if (rr < 0.8 * meanRR)
                         {
-                            if (ann[indAnn] > currentData[indData])
-                            {
-                                fp++;
-                                indData++;
-                                continue;
-                            }
-                            else
-                            {
-                                fn++;
-                                indAnn++;
-                                continue;
-                            }
+                            noOfOverdetectedPeaks++;
                         }
-                        else
+                        if (rr > 1.2 * meanRR)
                         {
-                            tp++;
-                            indAnn++;
-                            indData++;
+                            noOfUnderdetectedPeaks++;
                         }
                     }
-                    total = tp + fn;
-
-                    double sensitivity = Math.Round(tp*100.00 / (tp + fn),2);
-                    double positivePredictive = Math.Round(tp *100.00/ (tp + fp),2);
-
+                    
                     //add to stats output
-                    _strToStr.Add(_currentName + " Total: ", total.ToString());
-                    _strToObj.Add(_currentName + " Total: ", total);
-                    _strToStr.Add(_currentName + " True positives: ", tp.ToString());
-                    _strToObj.Add(_currentName + " True positives: ", tp);
-                    _strToStr.Add(_currentName + " False positives: ", fp.ToString());
-                    _strToObj.Add(_currentName + " False positives: ", fp);
-                    _strToStr.Add(_currentName + " False negatives: ", fn.ToString());
-                    _strToObj.Add(_currentName + " False negatives: ", fn);
-                    _strToStr.Add(_currentName + " Sensitivity: ", sensitivity.ToString());
-                    _strToObj.Add(_currentName + " Sensitivity: ", sensitivity);
-                    _strToStr.Add(_currentName + " Positive predictive: ", positivePredictive.ToString());
-                    _strToObj.Add(_currentName + " Positive predictive: ", positivePredictive);
+                    _strToStr.Add(_currentName + " Number of detected R-peaks: ", noOfPeaks.ToString());
+                    _strToObj.Add(_currentName + " Number of detected R-peaks: ", noOfPeaks);
+                    _strToStr.Add(_currentName + " Mean RR interval [ms]: ", meanRR.ToString());
+                    _strToObj.Add(_currentName + " Mean RR interval [ms]: ", meanRR);
+                    _strToStr.Add(_currentName + " Number of possible overdetected R-peaks: ", noOfOverdetectedPeaks.ToString());
+                    _strToObj.Add(_currentName + " Number of possible overdetected R-peaks: ", noOfOverdetectedPeaks);
+                    _strToStr.Add(_currentName + " Number of possible missed R-peaks: ", noOfUnderdetectedPeaks.ToString());
+                    _strToObj.Add(_currentName + " Number of possible missed R-peaks: ", noOfUnderdetectedPeaks);
+                    /* _strToStr.Add(_currentName + " Minimum distance between R peaks [ms]: ", minRR.ToString());
+                     _strToObj.Add(_currentName + " Minimum distance between R peaks [ms]: ", minRR);
+                     _strToStr.Add(_currentName + " Maximum distance between R peaks [ms]: ", maxRR.ToString());
+                     _strToObj.Add(_currentName + " Maximum distance between R peaks [ms]:", maxRR);*/
 
                     _currentState = State.NEXT_CHANNEL;
                     break;
