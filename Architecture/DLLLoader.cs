8e93e04b64898259ea5ed94d7789594d6a95008a
wodzinsk@student.agh.edu.pl <nefrius@gmail.com>
2016-01-17 11:39:53 +0100

    DLL Loader added.

---
+++ Architecture/DLLLoader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EKG_Project.IO;
+
+namespace EKG_Project.Architecture
+{
+    public class DLLLoader
+    {
+        public static void CopyWFDBDLL()
+        {
+            IECGPath pathBuilder = new DebugECGPath();
+            string basePath = pathBuilder.getBasePath();
+            string dllPath = System.IO.Path.Combine(basePath, "DLL");
+            string currentPath = pathBuilder.getCurrentPath();
+            string fileName = "wfdb.dll";
+            System.IO.File.Copy(System.IO.Path.Combine(dllPath, fileName), System.IO.Path.Combine(currentPath, fileName), true);
+        }
+    }
+}
