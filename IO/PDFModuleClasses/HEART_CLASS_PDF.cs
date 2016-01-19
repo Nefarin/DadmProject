dfc5cb12655127634f5a82d9b3c1a51541c05eca
KrzysztofKaganiec <campnou@student.agh.edu.pl>
2016-01-19 22:27:14 +0100

    PDF Module class - heart_class added

---
+++ IO/PDFModuleClasses/HEART_CLASS_PDF.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Shapes;
+using MigraDoc.DocumentObjectModel.Tables;
+
+namespace EKG_Project.IO.PDFModuleClasses
+{
+    class HEART_CLASS_PDF : IPDFModuleClass
+    {
+        public Document Document { get; set; }
+        public Section Section { get; set; }
+
+        public HEART_CLASS_PDF(Document document)
+        {
+            Document = document;
+            Section = Document.AddSection();
+            Section.AddPageBreak();
+        }
+
+        public void FillReportForModule(string _header)
+        {
+            Paragraph paragraph = Section.AddParagraph(_header);
+            paragraph.AddBookmark(_header);
+            paragraph.Format.SpaceBefore = "3cm";
+            paragraph.Format.Font.Size = 18;
+            paragraph.Format.Font.Bold = true;
+            paragraph.Format.SpaceAfter = 24;
+            paragraph.Format.OutlineLevel = OutlineLevel.Level1;
+        }
+        //public void IPDFModuleClass.fillReportForModule()
+        //{
+
+        //}
+    }
+}
