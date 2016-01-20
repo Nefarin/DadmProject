0f0828c05c31178f15f2667c0807941a8c4af167
KrzysztofKaganiec <campnou@student.agh.edu.pl>
2016-01-21 00:10:33 +0100

    table template function added

--- IO/PDF/Tables.cs
+++ IO/PDF/Tables.cs
@@ -2,61 +2,39 @@ using System;
 using MigraDoc.DocumentObjectModel;
 using MigraDoc.DocumentObjectModel.Shapes;
 using MigraDoc.DocumentObjectModel.Tables;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EKG_Project.IO
 {
-  public class Tables
+  public static class Tables
   {
-    public static void DefineTables(Document document)
+    public static void DefineTable(Document document, int _rows, int _cols, Dictionary<string, string> _strToStr)
     {
-        Paragraph paragraph = document.LastSection.AddParagraph("Table Overview", "Heading2");
-        paragraph.AddBookmark("Tables");
-        paragraph.Format.SpaceAfter = "2cm";
-        DemonstrateSimpleTable(document);
-        
+            Table table = new Table();
+            table.Borders.Width = 1;
 
-        Paragraph paragraph2 = document.LastSection.AddParagraph("Chart Overview", "Heading2");
-        paragraph2.Format.SpaceBefore = "3cm";
-        Image image = document.LastSection.AddImage("wykres.png");
-        image.Width = "15cm";
+            Column column;
+            Row row;
+            //Cell cell;
 
-  }
+            for (int count = 0; count < _cols; count++)
+            {
+                column = table.AddColumn(Unit.FromCentimeter(5));
+                column.Format.Alignment = ParagraphAlignment.Center;
+            }
 
-    public static void DemonstrateSimpleTable(Document document)
-    {
-      document.LastSection.AddParagraph("Simple Table", "Heading2");
+            for (int count = 0; count < _rows; count++)
+            {
+                row = table.AddRow();
+                var item = _strToStr.ElementAt(count);
+                row.Cells[0].AddParagraph(item.Key);
+                row.Cells[1].AddParagraph(item.Value);
+            }
 
-      Table table = new Table();
-      table.Borders.Width = 0.75;
+            table.SetEdge(0, 0, _cols, _rows, Edge.Box, BorderStyle.Single, 1.5, Colors.Black);
 
-      Column column = table.AddColumn(Unit.FromCentimeter(2));
-      column.Format.Alignment = ParagraphAlignment.Center;
-
-      table.AddColumn(Unit.FromCentimeter(5));
-
-      Row row = table.AddRow();
-      row.Shading.Color = Colors.PaleGoldenrod;
-      Cell cell = row.Cells[0];
-      cell.AddParagraph("Items");
-      cell = row.Cells[1];
-      cell.AddParagraph("Description");
-
-      row = table.AddRow();
-      cell = row.Cells[0];
-      cell.AddParagraph("Param 1");
-      cell = row.Cells[1];
-      cell.AddParagraph("Data 2");
-
-      row = table.AddRow();
-      cell = row.Cells[0];
-      cell.AddParagraph("Param 2");
-      cell = row.Cells[1];
-      cell.AddParagraph("Data 2");
-
-      table.SetEdge(0, 0, 2, 3, Edge.Box, BorderStyle.Single, 1.5, Colors.Black);
-
-      document.LastSection.Add(table);
-    }
-
+            document.LastSection.Add(table);
+        }
   }
 }
