28fec603231f6833e1a4915f1eadf9caffe89b68
Marek <nefrius@gmail.com>
2016-01-23 23:50:26 +0100

    Process of destruction - 1

--- EKG_Unit/Properties/AssemblyInfo.cs
+++ EKG_Unit/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EKG_ProjectTests")]
+[assembly: AssemblyTitle("EKG_Unit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EKG_ProjectTests")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("EKG_Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("51146378-57ed-4207-a846-fb0c8a081048")]
+[assembly: Guid("c42978f9-5551-4eb8-ab0d-8ef74ccbbc52")]
 
 // Version information for an assembly consists of the following four values:
 //
