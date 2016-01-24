cd58710f4b4582ab2320a14ef858b92ed5cbb787
ewakot <kotanskae@gmail.com>
2016-01-24 15:13:33 +0100

    poincare, tinn, ti

--- Modules/HRV2/Tinn.cs
+++ Modules/HRV2/Tinn.cs
@@ -2,25 +2,25 @@
 using System.Linq;
 using MathNet.Numerics.LinearAlgebra;
 
+
 namespace EKG_Project.Modules.HRV2
 {
 
     public partial class HRV2_Alg
     {
-
-        //private double tinn;
-        //#region Documentation
-        ///// <summary>
-        ///// współczynnik TINN czyli podstawa trójkąta dofitowanego do histogramu
-        ///// </summary>
-        ///// 
-        //#endregion
-        //private void makeTinn()
-        //{
-        //    Vector<double>  RRIntervals = InputData.RRInterval[_currentChannelIndex].Item2;
+        private double tinn;
+        #region Documentation
+        /// <summary>
+        /// Get the TINN coefficient, which is 
+        /// the base of the triangle fitted to histogram
+        /// </summary>
+        /// 
+        #endregion
+            private void makeTinn()
+        {
+        //    Vector<double> RRIntervals = InputData.RRInterval[_currentChannelIndex].Item2;
         //    double Tinn = (RRIntervals.Max() - RRIntervals.Min());
         //    tinn = Tinn;
-        //}
+        }
     }
- 
 }
--- Modules/HRV2/TriangleIndex.cs
+++ Modules/HRV2/TriangleIndex.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using MathNet.Numerics.LinearAlgebra;
 
+
 namespace EKG_Project.Modules.HRV2
 {
 
     public partial class HRV2_Alg
     {
-
-        //double triangleIndex;
-        //public void TriangleIndex()
-        //{
-        //    Vector <double> RRIntervaals = InputData.RRInterval[_outputIndex].Item2;
-        //    triangleIndex = _currentHistogram.MaxCount/_currentRPeaksLength;
-        //}
+        double triangleIndex;
+        #region Documentation
+        /// <summary>
+        /// Get Triangle Index coefficient, which is 
+        /// the value of the highest bin, devided by all RR intervals count
+        /// </summary>
+        /// 
+        #endregion
+        public void TriangleIndex()
+        {
+        //    Vector<double> RRIntervaals = InputData.RRInterval[_outputIndex].Item2;
+        //    triangleIndex = _currentHistogram.MaxCount / _currentRPeaksLength;
+        }
     }
 
 }
