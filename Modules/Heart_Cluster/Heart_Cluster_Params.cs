c05b3cd6d7a6429b81c3a897a6d6230a91b793d6
Marek <nefrius@gmail.com>
2016-01-26 23:00:30 +0100

    Added new module to project.. ^.^

---
+++ Modules/Heart_Cluster/Heart_Cluster_Params.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using EKG_Project.IO;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace EKG_Project.Modules.Heart_Cluster
+{
+    public class Heart_Cluster_Params : ModuleParams
+    {
+    }
+}
