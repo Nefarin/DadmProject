e4e55b9e711d2f7c46b692bc8e788e7eba8e15a4
Dobrochna Niezgoda <dobrochna.niezgoda@gmail.com>
2016-01-26 17:01:00 +0100

    Sleep_Apnea_Alg_Test

--- EKG_Unit/Modules/Sleep_Apnea/Sleep_Apnea_Alg_Test.cs
+++ EKG_Unit/Modules/Sleep_Apnea/Sleep_Apnea_Alg_Test.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MathNet.Numerics.LinearAlgebra;
+using EKG_Project.Modules.Sleep_Apnea;
+using System.Collections.Generic;
 
 
 namespace EKG_Unit.Modules.Sleep_Apnea
@@ -8,5 +10,32 @@ namespace EKG_Unit.Modules.Sleep_Apnea
     [TestClass]
     public class Sleep_Apnea_Alg_Test
     {
+        [TestMethod]
+        [Description("Test if rr intervals are calculated correctly")]
+        public void Sleep_Apnea_findIntervals_Test1()
+        {
+            // Init test here
+            Sleep_Apnea_Params testParams = new Sleep_Apnea_Params("Test");
+
+            int fs = 200;
+            List<uint> R_detected = new List<uint>() { 1, 2, 3, 4, 5, 6, 7 };
+
+            List<List<double>> RR_intervals = new List<List<double>>()
+            {
+                new List<double>() { 1,2,3,4,5,6,7},
+                new List<double>() {0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0}
+            };
+
+            Sleep_Apnea_Alg testAlgs = new Sleep_Apnea_Alg();
+            PrivateObject obj = new PrivateObject(testAlgs);
+
+            // Process test here
+            List<List<double>> result = (List<List<double>>)obj.Invoke("findIntervals", R_detected, fs);
+
+            // Assert results
+            CollectionAssert.AreEqual(RR_intervals[0], result[0]);
+            CollectionAssert.AreEqual(RR_intervals[1], result[1]);
+        }
+
     }
 }
