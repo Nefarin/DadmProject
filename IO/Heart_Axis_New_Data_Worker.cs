bc03f7a25a582ee551883888773f360f58151503
aniametz <aniametz@gmail.com>
2016-01-26 22:20:46 +0100

    Heart Axis worker

---
+++ IO/Heart_Axis_New_Data_Worker.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using MathNet.Numerics.LinearAlgebra;
+using EKG_Project.Modules;
+using System.Diagnostics;
+
+namespace EKG_Project.IO
+{
+    /// <summary>
+    /// Class that saves and loads Heart_Axis_Data from txt file
+    /// </summary>
+    class Heart_Axis_New_Data_Worker
+    {
+        //FIELDS
+        /// <summary>
+        /// Stores txt files directory
+        /// </summary>
+        private string directory;
+
+        /// <summary>
+        /// Stores analysis name
+        /// </summary>
+        private string analysisName;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Heart_Axis_New_Data_Worker() 
+        {
+            IECGPath pathBuilder = new DebugECGPath();
+            directory = pathBuilder.getTempPath();
+        }
+
+        /// <summary>
+        /// Parameterized constructor
+        /// </summary>
+        /// <param name="analysisName">analysis name</param>
+        public Heart_Axis_New_Data_Worker(String analysisName) : this()
+        {
+            this.analysisName = analysisName;
+        }
+
+        /// <summary>
+        /// Saves HeartAxis in txt file
+        /// </summary>
+        /// <param name="value">HeartAxis</param>
+        public void SaveAttribute(double value)
+        {
+            string moduleName = this.GetType().Name;
+            moduleName = moduleName.Replace("_Data_Worker", "");
+            string fileName = analysisName + "_" + moduleName + ".txt";
+            string pathOut = System.IO.Path.Combine(directory, fileName);
+
+            StreamWriter sw = new StreamWriter(pathOut);
+            sw.WriteLine(value);
+            sw.Close();
+        }
+
+        /// <summary>
+        /// Loads HeartAxis from txt file
+        /// </summary>
+        /// <returns>HeartAxis</returns>
+        public double LoadAttribute()
+        {
+            string moduleName = this.GetType().Name;
+            moduleName = moduleName.Replace("_Data_Worker", "");
+            string fileName = analysisName + "_" + moduleName + ".txt";
+            string pathIn = System.IO.Path.Combine(directory, fileName);
+
+            StreamReader sr = new StreamReader(pathIn);
+            string readLine = sr.ReadLine();
+            sr.Close();
+
+            double readValue = Convert.ToDouble(readLine);
+            return readValue;
+        }
+    }
+}
