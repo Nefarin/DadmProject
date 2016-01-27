836476c687820dc10645409ba8385b5fa2447c5b
Paulina Soltys <soltys221@gmail.com>
2016-01-27 17:38:12 +0100

    Slightly changes, too much code to test

--- EKG_Unit/Modules/HRV_DFA/HRV_DFA_Alg_Test.cs
+++ EKG_Unit/Modules/HRV_DFA/HRV_DFA_Alg_Test.cs
@@ -7,5 +7,13 @@ namespace EKG_Unit.Modules.HRV_DFA
     [TestClass]
     public class HRV_DFA_Alg_Test
     {
+        //Test for subsidary methods
+        [TestMethod]
+        [Description("Test if RemoveZeros works properly")]
+        public void RemoveZerosTest()
+        {
+            HRV_DFA
+        }
+
     }
 }
