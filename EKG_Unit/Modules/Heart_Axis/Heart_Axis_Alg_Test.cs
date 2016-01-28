90ba2d4b51fe542618a0046bfa260c754e5ad247
Agnieszka Kajda <agnkajda@student.agh.edu.pl>
2016-01-28 23:43:04 +0100

    tests

--- EKG_Unit/Modules/Heart_Axis/Heart_Axis_Alg_Test.cs
+++ EKG_Unit/Modules/Heart_Axis/Heart_Axis_Alg_Test.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MathNet.Numerics.LinearAlgebra;
+using EKG_Project.Modules.Heart_Axis;
 
 
 namespace EKG_Unit.Modules.Heart_Axis
@@ -8,5 +9,42 @@ namespace EKG_Unit.Modules.Heart_Axis
     [TestClass]
     public class Heart_Axis_Alg_Test
     {
+        [TestMethod]
+        [Description("Test if pseudomodule works properly")]
+        public void pseudomoduleTest()
+        {
+            Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
+
+            double[] testArray = { 3, 4, 3, 4, 3 };
+            double[] resultArray = { 5, 5, 5, 5 };
+
+
+            Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
+            PrivateObject obj = new PrivateObject(testAlgs);
+            object[] args = {0, 4, testArray};
+            obj.Invoke("PseudoModule", args);
+
+            Assert.AreNotEqual(testAlgs, resultArray);
+
+        }
+
+        [TestMethod]
+        [Description("Test if method finds maximum")]
+        public void maxOfPsudomoduleTest()
+        {
+            Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
+
+            double[] testArray = { 1, 2, 8, 1, 6 };
+            double[] resultArray = { 2 };
+
+
+            Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
+            PrivateObject obj = new PrivateObject(testAlgs);
+            object[] args = { 0, testArray };
+            obj.Invoke("MaxOfPseudoModule", args);
+
+            Assert.AreNotEqual(testAlgs, resultArray);
+
+        }
     }
 }
