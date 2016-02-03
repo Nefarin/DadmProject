538d82e2f5bfda23bf175062f0529c78e785029c
Ewelina Blicharz <eweliblicharz@gmail.com>
2016-02-03 15:01:23 +0100

    stats v.1

--- Modules/HRV2/HRV2_Stats.cs
+++ Modules/HRV2/HRV2_Stats.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using EKG_Project.IO;
-using EKG_Project.Modules.R_Peaks;
-using System.Text;
-using System.Threading.Tasks;
-
+using MathNet.Numerics.LinearAlgebra;
 
 namespace EKG_Project.Modules.HRV2
 {
@@ -17,10 +13,12 @@ namespace EKG_Project.Modules.HRV2
         private Dictionary<string, Object> _strToObj;
         private Dictionary<string, string> _strToStr;
         private string _analysisName;
-        private HRV2_Data _data;
         private State _currentState;
         private int _currentChannelIndex;
+        private int _currentIndex;
         private string _currentName;
+        private Basic_New_Data_Worker _worker;
+        private string[] _leads;
 
 
         public void Abort()
@@ -61,53 +59,50 @@ namespace EKG_Project.Modules.HRV2
             _strToObj = new Dictionary<string, object>();
             _strToStr = new Dictionary<string, string>();
 
-            HRV2_Data_Worker worker = new HRV2_Data_Worker(_analysisName);
-            worker.Load();
-            _data = worker.Data;
+            _worker = new Basic_New_Data_Worker(_analysisName);
+            _leads = _worker.LoadLeads().ToArray();
             _currentState = State.START_CHANNEL;
             _currentChannelIndex = 0;
         }
 
-        //a se tym razem to jako maszyne stanów zrobimy i pominiemy step, bo to sie powinno liczyc duzo szybciej niż moduły,
-        // jedynie odprowadzenia podzielmy - wasze powinny byc jednak zdecydowanie bardziej skomplikowane
         public void ProcessStats()
         {
-            //switch (_currentState)
-            //{
-            //    case (State.START_CHANNEL):
-            //        _currentName = _data.Output[_currentChannelIndex].Item1;
-            //        _currentState = State.CALCULATE;
-            //        break;
-            //    case (State.CALCULATE):
-            //        Vector<double> currentData = _data.Output[_currentChannelIndex].Item2;
-            //        double mean = currentData.Sum() / currentData.Count;
-            //        _strToStr.Add(_currentName + " mean value: ", mean.ToString()); //generalnie to jakie statystyki wasz moduł powinien wyznacać zależy przede wszystkim od was
-            //        _strToObj.Add(_currentName + " mean value: ", mean);            //klucz nie jest tak ważny, bo i tak można je wszystkie później wyciągnąć automatycznie
-            //                                                                        //jeżeli coś może się liczyć dłużej to musicie podzielić również sygnał na części
-            //                                                                        // zależnie od modułu powinny sie tu znaleźć średnie, wariancje, odchylenia, ilości dobrze/źle
-            //                                                                        // wykrytych rzeczy, jakieś porównanie klasyfikacji etc. - możliwe, że niektóre moduły zostawią
-            //                                                                        // tą funkcje po prostu pustą (chociaż zdecydowana większość powinna ją uzupełnić - narazie jedynymi
-            //                                                                        // wyjątkami, które widze są Heart_Axis i ECG_Baseline
-            //        _currentState = State.NEXT_CHANNEL;
-            //        break;
-            //    case (State.NEXT_CHANNEL):
-            //        _currentChannelIndex++;
-            //        if (_currentChannelIndex >= _data.Output.Count)
-            //        {
-            //            _currentState = State.END;
-            //        }
-            //        else _currentState = State.START_CHANNEL;
-            //        break;
-            //    case (State.END):
-            //        _ended = true;
-            //        break;
-            //}
+            switch (_currentState)
+            {
+                case (State.START_CHANNEL):
+                    _currentName = _leads[_currentChannelIndex];
+                    _currentIndex = 0;
+                    _currentState = State.CALCULATE;
+                    break;
+                case (State.CALCULATE):
+                    // Podstawowe załozenie - ECG_Baseline nie generuje statystyk (generuje puste) i zakladamy, ze sygnal EKG bedzie krotszy niz okolo 150 dni
+                    // w takim przypadku statystyki dla pozostalych modulow bez problemu powinny sie zmiescic w pamieci przetwarzajac odprowadzenie po odprowadzeniu
+                    // jezeli z jakiegos powodu sygnal mialby byc dluzszy niz te prawie pol roku - trzeba dorobic dodatkowe IO do statsow, co z racji na brak czasu - pomijamy
+                    // Zalozenie jest o tyle sensowne, ze sygnal 150 dni przy obecnej predkosci dzialania modulow liczylby sie okolo 37.5 godziny..
+                    Vector<double> currentData = _worker.LoadSignal(_currentName, 0, (int)_worker.getNumberOfSamples(_currentName));
+                    double mean = currentData.Sum() / currentData.Count;
+                    _strToStr.Add(_currentName + " mean value: ", mean.ToString());
+                    _strToObj.Add(_currentName + " mean value: ", mean);
+                    _currentState = State.NEXT_CHANNEL;
+                    break;
+                case (State.NEXT_CHANNEL):
+                    _currentChannelIndex++;
+                    if (_currentChannelIndex >= _leads.Length) _currentState = State.END;
+                    else
+                    {
+                        _currentState = State.START_CHANNEL;
+                    }
+                    break;
+                case (State.END):
+                    _ended = true;
+                    break;
+            }
         }
 
         public static void Main(String[] args)
         {
             HRV2_Stats stats = new HRV2_Stats();
-            stats.Init("Analysis6");
+            stats.Init("abc123");
 
 
             while (true)
