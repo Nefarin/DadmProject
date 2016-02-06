483f77409a3a77a8641d13a5d7b71b1a5890f5ea
Agnieszka Kajda <agnkajda@student.agh.edu.pl>
2016-02-06 19:32:30 +0100

    tests done

--- EKG_Unit/Modules/Heart_Axis/Heart_Axis_Alg_Test.cs
+++ EKG_Unit/Modules/Heart_Axis/Heart_Axis_Alg_Test.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MathNet.Numerics.LinearAlgebra;
 using EKG_Project.Modules.Heart_Axis;
+using System.Reflection;
 
 
 namespace EKG_Unit.Modules.Heart_Axis
@@ -21,10 +22,9 @@ namespace EKG_Unit.Modules.Heart_Axis
 
             Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
             PrivateObject obj = new PrivateObject(testAlgs);
-            object[] args = {0, 4, testArray};
-            obj.Invoke("PseudoModule", args);
+            double[] realresultArray = testAlgs.PseudoModule(0, 4, testArray);
 
-            Assert.AreNotEqual(testAlgs, resultArray);
+            CollectionAssert.AreEquivalent(realresultArray, resultArray);
 
         }
 
@@ -35,16 +35,221 @@ namespace EKG_Unit.Modules.Heart_Axis
             Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
 
             double[] testArray = { 1, 2, 8, 1, 6 };
-            double[] resultArray = { 2 };
+            int result = 2;
 
 
             Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
             PrivateObject obj = new PrivateObject(testAlgs);
-            object[] args = { 0, testArray };
-            obj.Invoke("MaxOfPseudoModule", args);
+            int realresult = testAlgs.MaxOfPseudoModule(0, testArray);
 
-            Assert.AreNotEqual(testAlgs, resultArray);
+            Assert.AreEqual(realresult, result);
 
         }
-    }
+
+        [TestMethod]
+        [Description("Test if method finds maximum when all arguments are equal")]
+        public void maxOfPsudomoduleTest2()
+        {
+            Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
+
+            double[] testArray = { 2, 2, 2, 2, 2 };
+            int result = 0;
+
+
+            Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
+            PrivateObject obj = new PrivateObject(testAlgs);
+            int realresult = testAlgs.MaxOfPseudoModule(0, testArray);
+
+            Assert.AreEqual(realresult, result);
+
+        }
+
+        [TestMethod]
+        [Description("LeastSquaresMethod - A")] 
+        public void leastSquaresMethodTestA()
+        {
+            Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
+
+            double[] testArray = { 4, 7, 8, 7, 4 };
+            double[] testArray2 = { 1, 1, 8, 1, 1 };
+            double[] resultArray = { 4, 4, -1 };
+
+
+            Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
+            double[] realresultArray = testAlgs.LeastSquaresMethod(testArray, 0, testArray2, 100);
+
+            Assert.AreEqual(realresultArray[2], resultArray[2],0.1);
+
+        }
+
+        [TestMethod]
+        [Description("LeastSquaresMethod - B")]
+        public void leastSquaresMethodTestB()
+        {
+            Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
+
+            double[] testArray = { 4, 7, 8, 7, 4 };
+            double[] testArray2 = { 1, 1, 8, 1, 1 };
+            double[] resultArray = { 4, 4, -1 };
+
+
+            Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
+            double[] realresultArray = testAlgs.LeastSquaresMethod(testArray, 0, testArray2, 100);
+
+            //CollectionAssert.AreNotEqual(realresultArray, resultArray);
+            Assert.AreEqual(realresultArray[1], resultArray[1]);
+
+
+
+        }
+
+
+        [TestMethod]
+        [Description("LeastSquaresMethod - C")]
+        public void leastSquaresMethodTestC()
+        {
+            Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
+
+            double[] testArray = { 4, 7, 8, 7, 4 };
+            double[] testArray2 = { 1, 1, 8, 1, 1 };
+            double[] resultArray = { 4, 4, -1 };
+
+
+            Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
+            double[] realresultArray = testAlgs.LeastSquaresMethod(testArray, 0, testArray2, 100);
+
+            Assert.AreEqual(realresultArray[0], resultArray[0]);
+
+
+
+        }
+
+
+        [TestMethod]
+        [Description("LeastSquaresMethod - when signal is too short")]
+        public void leastSquaresMethodTest2()
+        {
+            Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
+
+            double[] testArray = { 4, 7, 8, 7, 4 };
+            double[] testArray2 = { 5, 1, 2, 1, 1 };
+            double[] resultArray = { 0, 0, 0};
+
+
+            Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
+            double[] realresultArray = testAlgs.LeastSquaresMethod(testArray, 0, testArray2, 100);
+
+            CollectionAssert.AreEquivalent(realresultArray, resultArray);
+
+        }
+
+        [TestMethod]
+        [Description("Test if method MaxOfPolynomial calculates properly")]
+        public void maxOfPolynomialTest()
+        {
+            Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
+
+            double[] testArray = { 12, 8, -4 };
+            int result = 1;
+
+
+            Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
+            PrivateObject obj = new PrivateObject(testAlgs);
+            int realresult = testAlgs.MaxOfPolynomial(0, testArray);
+            Assert.AreEqual(realresult, result);
+
+        }
+
+        [TestMethod]
+        [Description("MaxOfPolynomial - dividing by 0")]
+        public void maxOfPolynomialTest2()
+        {
+            Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
+
+            double[] testArray = { 1, 8, 0 };
+            int result = 0;
+
+
+            Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
+            PrivateObject obj = new PrivateObject(testAlgs);
+            int realresult = testAlgs.MaxOfPolynomial(0, testArray);
+            Assert.AreEqual(realresult, result);
+
+        }
+
+
+        [TestMethod]
+        [Description("ReadingAmplitudes - checks if works properly")]
+        public void readingAmplitudesTest()
+        {
+            Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
+
+            double[] testArray1 = { 5, 4, 3, 2, 1 };
+            double[] testArray2 = { 1, 2, 3, 4, 5 };
+            double[] resultArray = { 4, 2 };
+
+
+            Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
+            PrivateObject obj = new PrivateObject(testAlgs);
+            double[] realresultArray = testAlgs.ReadingAmplitudes(testArray1, testArray2, 1);
+
+
+            CollectionAssert.AreEquivalent(realresultArray, resultArray);
+
+        }
+
+        [TestMethod]
+        [Description("ReadingAmplitudes - while index equals 0")]
+        public void readingAmplitudesTest2()
+        {
+            Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
+
+            double[] testArray1 = { 5, 4, 3, 2, 1 };
+            double[] testArray2 = { 1, 2, 3, 4, 5 };
+            double[] resultArray = { 0, 0 };
+
+
+            Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
+            PrivateObject obj = new PrivateObject(testAlgs);
+            double[] realresultArray = testAlgs.ReadingAmplitudes(testArray1, testArray2, 0);
+
+
+            CollectionAssert.AreEquivalent(realresultArray, resultArray);
+
+        }
+
+
+        [TestMethod]
+        [Description("IandII - checks if works properly")]
+        public void IandIITest()
+        {
+            Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
+
+            double[] testArray = { 2, 5};
+            double result = 1.047;
+
+
+            Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
+            PrivateObject obj = new PrivateObject(testAlgs);
+            object[] args = { testArray };
+            obj.Invoke("IandII", args);
+            double realresult = Convert.ToDouble(obj.Invoke("IandII", args));
+            Assert.AreEqual(result, realresult, 0.001);
+
+        }
+
+        [TestMethod]
+        [Description("IandII - dividing by 0")]
+        public void IandIITest2()
+        {
+            Heart_Axis_Params testParams = new Heart_Axis_Params("Test");
+            double[] testArray = { 0, 2 };
+            double result = 0;
+            Heart_Axis_Alg testAlgs = new Heart_Axis_Alg();
+            double realresult = testAlgs.IandII(testArray);
+
+            Assert.AreEqual( result, realresult);
+        }
+
+        }
 }
