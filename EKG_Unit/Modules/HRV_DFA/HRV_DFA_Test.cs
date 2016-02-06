d066cefec860eb35c0caa6f5750ffe74a99dad30
Paulina Soltys <soltys221@gmail.com>
2016-02-06 22:21:53 +0100

    dondondondone

--- EKG_Unit/Modules/HRV_DFA/HRV_DFA_Test.cs
+++ EKG_Unit/Modules/HRV_DFA/HRV_DFA_Test.cs
@@ -18,7 +18,7 @@ namespace EKG_Unit.Modules.HRV_DFA
         public void GeneralInterface_Test()
         {
             IModule testModule = new EKG_Project.Modules.HRV_DFA.HRV_DFA();
-            HRV_DFA_Params test_params = new HRV_DFA_Params("Analysis 1");
+            HRV_DFA_Params test_params = new HRV_DFA_Params("a100dat");
 
             testModule.Init(test_params);
             int counter = 0;
@@ -30,6 +30,32 @@ namespace EKG_Unit.Modules.HRV_DFA
                 testModule.Abort();
             }
             Assert.AreEqual(1, counter);
+        }
+        [TestMethod]
+        [Description("Test if interface performs in fast process state")]
+        public void Interface_FastProcessTest()
+        {
+            IModule testModule = new EKG_Project.Modules.HRV_DFA.HRV_DFA();
+            HRV_DFA_Params test_params = new HRV_DFA_Params("a100dat");
+            testModule.Init(test_params);
+
+            int counter = 0;
+            while (!testModule.Ended())
+            {
+                counter++;
+                Debug.WriteLine(testModule.Progress());
+                testModule.ProcessData();
+            }
+            Basic_New_Data_Worker _basicWorker = new Basic_New_Data_Worker("a100dat");
+            R_Peaks_New_Data_Worker _rpeaksWorker = new R_Peaks_New_Data_Worker("a100dat");
+
+            HRV_DFA_New_Data_Worker _worker = new HRV_DFA_New_Data_Worker("a100dat");
+            string[] leads = _basicWorker.LoadLeads().ToArray();
+            Vector<double> testVector = _rpeaksWorker.LoadSignal(R_Peaks_Attributes.RRInterval, leads[0], 0, (int)_rpeaksWorker.getNumberOfSamples(R_Peaks_Attributes.RRInterval, leads[0]));
+
+            Tuple<Vector<double>, Vector<double>> results = _worker.LoadSignal(HRV_DFA_Signals.DfaValueFn, leads[0], 0, (int)_worker.getNumberOfSamples(HRV_DFA_Signals.ParamAlpha, leads[0]));
+            Assert.IsNotNull(results.Item1);
+
         }
 
     }
