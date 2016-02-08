abc07d8c58d247f4b3c39e5b7b5986498d9de0fa
wodzinsk@student.agh.edu.pl <nefrius@gmail.com>
2016-02-08 20:16:24 +0100

    Heart_Cluster included in GUI.

--- Architecture/Modules.cs
+++ Architecture/Modules.cs
@@ -39,7 +39,7 @@ namespace EKG_Project.Architecture
             AvailableOptions.WAVES,
             AvailableOptions.FLUTTER,
             //AvailableOptions.SIG_EDR,
-            AvailableOptions.ST_SEGMENT,
+            //AvailableOptions.ST_SEGMENT,
             AvailableOptions.SLEEP_APNEA,
             AvailableOptions.ATRIAL_FIBER,
             AvailableOptions.QT_DISP,
@@ -48,6 +48,7 @@ namespace EKG_Project.Architecture
             AvailableOptions.HEART_CLASS,
             AvailableOptions.HRT,
             AvailableOptions.HEART_AXIS,
+            AvailableOptions.HEART_CLUSTER,
             //AvailableOptions.TEST_MODULE
         };
 
